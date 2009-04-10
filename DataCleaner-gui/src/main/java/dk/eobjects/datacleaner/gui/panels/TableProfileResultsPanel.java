43339335bca6e6e7651e321be029f12e3ed652f7
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2009-04-10 00:24:57 +0000

    Ticket #284: Added drop-down for exporting profile results in gui.

--- DataCleaner-gui/src/main/java/dk/eobjects/datacleaner/gui/panels/TableProfileResultsPanel.java
+++ DataCleaner-gui/src/main/java/dk/eobjects/datacleaner/gui/panels/TableProfileResultsPanel.java
@@ -30,14 +30,18 @@ import java.util.Map.Entry;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JToolBar;
 
 import org.jdesktop.swingx.JXTaskPane;
 import org.jdesktop.swingx.JXTaskPaneContainer;
 
+import dk.eobjects.datacleaner.export.HtmlResultExporter;
+import dk.eobjects.datacleaner.export.IResultExporter;
 import dk.eobjects.datacleaner.export.XmlResultExporter;
 import dk.eobjects.datacleaner.gui.GuiHelper;
 import dk.eobjects.datacleaner.gui.widgets.MatrixTable;
@@ -52,51 +56,41 @@ import dk.eobjects.metamodel.util.FileHelper;
 public class TableProfileResultsPanel extends JPanel {
 
 	private static final long serialVersionUID = 7504166314384076977L;
+	private Table _table;
+	private List<IProfileResult> _results;
 
-	public TableProfileResultsPanel(DataContext dataContext, final Table table,
-			final List<IProfileResult> results) {
+	public TableProfileResultsPanel(DataContext dataContext, final Table table, final List<IProfileResult> results) {
 		super();
+		_table = table;
+		_results = results;
 		setLayout(new BorderLayout());
 
-		JLabel headerLabel = new JLabel("Profiler results for "
-				+ table.getName());
+		JLabel headerLabel = new JLabel("Profiler results for " + table.getName());
 		headerLabel.setFont(GuiHelper.FONT_HEADER);
 
-		JButton exportButton = new JButton("Export", GuiHelper
-				.getImageIcon("images/toolbar_save.png"));
+		JButton exportButton = new JButton("Export", GuiHelper.getImageIcon("images/toolbar_save.png"));
 		exportButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JFileChooser f = new JFileChooser();
-				f.setSelectedFile(new File(table.getName() + "-results.xml"));
-				if (JFileChooser.APPROVE_OPTION == f
-						.showSaveDialog(TableProfileResultsPanel.this)) {
-					boolean accepted = false;
-					File file = f.getSelectedFile();
-					if (file.exists()) {
-						if (JOptionPane.YES_OPTION == JOptionPane
-								.showConfirmDialog(
-										TableProfileResultsPanel.this,
-										"File already exists, overwrite?",
-										"Overwrite", JOptionPane.YES_NO_OPTION)) {
-							accepted = true;
-						}
-					} else {
-						accepted = true;
+				JPopupMenu popupMenu = new JPopupMenu();
+				JMenuItem xmlMenuItem = new JMenuItem("XML");
+				xmlMenuItem.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						exportToFile(new XmlResultExporter(), "xml");
 					}
-					if (accepted) {
-						PrintWriter writer = new PrintWriter(FileHelper
-								.getBufferedWriter(file));
-						XmlResultExporter xmlResultExporter = new XmlResultExporter();
-						xmlResultExporter.writeProfileResultHeader(writer);
-						for (IProfileResult profileResult : results) {
-							xmlResultExporter.writeProfileResult(table,
-									profileResult, writer);
-						}
-						xmlResultExporter.writeProfileResultFooter(writer);
-						writer.close();
+				});
+				popupMenu.add(xmlMenuItem);
+
+				JMenuItem htmlMenuItem = new JMenuItem("HTML");
+				htmlMenuItem.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						exportToFile(new HtmlResultExporter(), "html");
 					}
-				}
+				});
+				popupMenu.add(htmlMenuItem);
+				JButton button = (JButton) e.getSource();
+				popupMenu.show(button, 0, button.getHeight());
 			}
+
 		});
 
 		JToolBar toolBar = GuiHelper.createToolBar();
@@ -110,27 +104,23 @@ public class TableProfileResultsPanel extends JPanel {
 		// Loop through every profile for this table
 		Map<IProfileDescriptor, List<IProfileResult>> profiles = ProfilerHelper
 				.getProfileResultsByProfileDescriptor(results);
-		for (Entry<IProfileDescriptor, List<IProfileResult>> entry : profiles
-				.entrySet()) {
+		for (Entry<IProfileDescriptor, List<IProfileResult>> entry : profiles.entrySet()) {
 			IProfileDescriptor profileDescriptor = entry.getKey();
 			List<IProfileResult> profileResults = entry.getValue();
 			String displayName = profileDescriptor.getDisplayName();
 			int matrixNumber = 0;
 
-			for (Iterator<IProfileResult> it = profileResults.iterator(); it
-					.hasNext();) {
+			for (Iterator<IProfileResult> it = profileResults.iterator(); it.hasNext();) {
 				IProfileResult profileResult = it.next();
 				Exception error = profileResult.getError();
 				if (error != null) {
 					JXTaskPane taskPane = new JXTaskPane();
-					taskPane.setIcon(GuiHelper.getImageIcon(profileDescriptor
-							.getIconPath()));
+					taskPane.setIcon(GuiHelper.getImageIcon(profileDescriptor.getIconPath()));
 					taskPane.setAnimated(false);
 					taskPane.setCollapsed(false);
 					taskPane.setTitle(displayName);
 					JLabel errorLabel = new JLabel(error.getMessage());
-					errorLabel.setFont(errorLabel.getFont().deriveFont(
-							Font.BOLD));
+					errorLabel.setFont(errorLabel.getFont().deriveFont(Font.BOLD));
 					taskPane.add(errorLabel);
 					taskPane.add(new JLabel("See the log for details."));
 					taskPaneContainer.add(taskPane);
@@ -139,8 +129,7 @@ public class TableProfileResultsPanel extends JPanel {
 					for (int j = 0; j < matrices.length; j++) {
 						matrixNumber++;
 						JXTaskPane taskPane = new JXTaskPane();
-						taskPane.setIcon(GuiHelper
-								.getImageIcon(profileDescriptor.getIconPath()));
+						taskPane.setIcon(GuiHelper.getImageIcon(profileDescriptor.getIconPath()));
 						taskPane.setAnimated(false);
 						taskPane.setCollapsed(false);
 						if (matrices.length > 1) {
@@ -151,18 +140,15 @@ public class TableProfileResultsPanel extends JPanel {
 						String[] columnNames = matrix.getColumnNames();
 						if (matrices.length > 1) {
 							if (columnNames.length == 1) {
-								taskPane.setTitle(displayName + " ("
-										+ columnNames[0] + ")");
+								taskPane.setTitle(displayName + " (" + columnNames[0] + ")");
 							} else {
-								taskPane.setTitle(displayName + " #"
-										+ matrixNumber);
+								taskPane.setTitle(displayName + " #" + matrixNumber);
 							}
 						} else {
 							taskPane.setTitle(displayName);
 						}
 
-						MatrixTable matrixTable = new MatrixTable(matrix,
-								dataContext);
+						MatrixTable matrixTable = new MatrixTable(matrix, dataContext);
 						taskPane.add(matrixTable.toPanel());
 						taskPaneContainer.add(taskPane);
 					}
@@ -170,5 +156,31 @@ public class TableProfileResultsPanel extends JPanel {
 			}
 		}
 		add(new JScrollPane(taskPaneContainer), BorderLayout.CENTER);
+	}
+
+	private void exportToFile(IResultExporter resultExporter, String fileExtension) {
+		JFileChooser f = new JFileChooser();
+		f.setSelectedFile(new File(_table.getName() + "-results." + fileExtension));
+		if (JFileChooser.APPROVE_OPTION == f.showSaveDialog(TableProfileResultsPanel.this)) {
+			boolean accepted = false;
+			File file = f.getSelectedFile();
+			if (file.exists()) {
+				if (JOptionPane.YES_OPTION == JOptionPane.showConfirmDialog(TableProfileResultsPanel.this,
+						"File already exists, overwrite?", "Overwrite", JOptionPane.YES_NO_OPTION)) {
+					accepted = true;
+				}
+			} else {
+				accepted = true;
+			}
+			if (accepted) {
+				PrintWriter writer = new PrintWriter(FileHelper.getBufferedWriter(file));
+				resultExporter.writeProfileResultHeader(writer);
+				for (IProfileResult profileResult : _results) {
+					resultExporter.writeProfileResult(_table, profileResult, writer);
+				}
+				resultExporter.writeProfileResultFooter(writer);
+				writer.close();
+			}
+		}
 	}
 }

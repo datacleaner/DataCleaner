554cfd05102ac789d284cc2d050bb7e3ec76c18a
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2009-07-17 22:42:33 +0000

    Updated POM versions to make "mvn site" possible

--- DataCleaner-gui/src/test/java/dk/eobjects/datacleaner/gui/VersionTest.java
+++ DataCleaner-gui/src/test/java/dk/eobjects/datacleaner/gui/VersionTest.java
@@ -17,6 +17,9 @@
 package dk.eobjects.datacleaner.gui;
 
 import java.io.File;
+import java.io.FileFilter;
+
+import javax.xml.parsers.DocumentBuilder;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
@@ -27,12 +30,51 @@ import dk.eobjects.datacleaner.util.DomHelper;
 public class VersionTest extends DataCleanerTestCase {
 
 	public void testVersionConsistency() throws Exception {
-		File pomFile = new File("../pom.xml");
-		Document document = DomHelper.getDocumentBuilder().parse(pomFile);
-		Node projectNode = DomHelper.getChildNodesByName(document, "project").get(0);
-		Node propertiesNode = DomHelper.getChildNodesByName(projectNode, "properties").get(0);
-		String pomVersion = DomHelper.getChildNodeText(propertiesNode, "datacleaner.version");
-		
+		DocumentBuilder documentBuilder = DomHelper.getDocumentBuilder();
+
+		File parentPomFile = new File("../pom.xml");
+		Document document = documentBuilder.parse(parentPomFile);
+		Node projectNode = DomHelper.getChildNodesByName(document, "project")
+				.get(0);
+		String pomVersion = DomHelper.getChildNodeText(projectNode, "version");
+
+		// Assert that the UI's version number is the same as the build tool's
+		// version number
 		assertEquals(DataCleanerGui.VERSION, pomVersion);
+
+		File parentDirectory = parentPomFile.getParentFile();
+		File[] moduleDirs = parentDirectory.listFiles(new FileFilter() {
+			public boolean accept(File file) {
+				return file.isDirectory();
+			}
+		});
+
+		int verifiedPomFiles = 0;
+		for (File moduleDir : moduleDirs) {
+			File modulePomFile = new File(moduleDir.getAbsolutePath()
+					+ File.separatorChar + "pom.xml");
+			if (modulePomFile.exists()) {
+				document = documentBuilder.parse(modulePomFile);
+				projectNode = DomHelper
+						.getChildNodesByName(document, "project").get(0);
+				pomVersion = DomHelper.getChildNodeText(projectNode, "version");
+
+				// Assert that all module versions are references to the
+				// parent's version
+				assertEquals("${parent.version}", pomVersion);
+
+				Node parentNode = DomHelper.getChildNodesByName(projectNode,
+						"parent").get(0);
+				pomVersion = DomHelper.getChildNodeText(parentNode, "version");
+
+				// Assert that all <parent> entries contain the same version as
+				// the parent POM.
+				assertEquals(DataCleanerGui.VERSION, pomVersion);
+
+				verifiedPomFiles++;
+			}
+		}
+
+		assertEquals(4, verifiedPomFiles);
 	}
 }

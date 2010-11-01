30c3c07401ab91c1115c2172bde59395dc75fca1
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2010-11-01 10:18:26 +0000

    Improved display of ListResults

---
+++ src/main/java/org/eobjects/datacleaner/widgets/result/ListResultSwingRenderer.java
@@ -0,0 +1,29 @@
+package org.eobjects.datacleaner.widgets.result;
+
+import java.util.List;
+
+import javax.swing.JComponent;
+import javax.swing.table.DefaultTableModel;
+
+import org.eobjects.analyzer.beans.api.RendererBean;
+import org.eobjects.analyzer.result.ListResult;
+import org.eobjects.analyzer.result.renderer.Renderer;
+import org.eobjects.analyzer.result.renderer.SwingRenderingFormat;
+import org.eobjects.datacleaner.widgets.table.DCTable;
+
+@RendererBean(SwingRenderingFormat.class)
+public class ListResultSwingRenderer implements Renderer<ListResult<?>, JComponent> {
+
+	@Override
+	public JComponent render(ListResult<?> result) {
+		List<?> values = result.getValues();
+		DefaultTableModel model = new DefaultTableModel(new String[] { "Values" }, values.size());
+		int i = 0;
+		for (Object object : values) {
+			model.setValueAt(object, i, 0);
+			i++;
+		}
+		return new DCTable(model).toPanel();
+	}
+
+}

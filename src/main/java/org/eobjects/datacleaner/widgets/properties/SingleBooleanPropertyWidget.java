23b181219d850b0c5c2c84834bfa7ec5a230764d
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2010-11-17 14:13:44 +0000

    Improved widget for optional boolean properties

--- src/main/java/org/eobjects/datacleaner/widgets/properties/SingleBooleanPropertyWidget.java
+++ src/main/java/org/eobjects/datacleaner/widgets/properties/SingleBooleanPropertyWidget.java
@@ -19,53 +19,110 @@
  */
 package org.eobjects.datacleaner.widgets.properties;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 import org.eobjects.analyzer.descriptors.ConfiguredPropertyDescriptor;
 import org.eobjects.analyzer.job.builder.AbstractBeanJobBuilder;
+import org.eobjects.datacleaner.util.LabelConstants;
 
 public class SingleBooleanPropertyWidget extends AbstractPropertyWidget<Boolean> {
 
 	private static final long serialVersionUID = 1L;
 
 	private final JCheckBox _checkBox;
+	private final JComboBox _comboBox;
 
 	public SingleBooleanPropertyWidget(ConfiguredPropertyDescriptor propertyDescriptor,
 			AbstractBeanJobBuilder<?, ?, ?> beanJobBuilder) {
 		super(beanJobBuilder, propertyDescriptor);
-		_checkBox = new JCheckBox();
+
 		Boolean currentValue = (Boolean) beanJobBuilder.getConfiguredProperty(propertyDescriptor);
-		_checkBox.setOpaque(false);
-		if (currentValue != null) {
-			_checkBox.setSelected(currentValue.booleanValue());
-		}
-		_checkBox.addChangeListener(new ChangeListener() {
-			@Override
-			public void stateChanged(ChangeEvent e) {
-				fireValueChanged();
+
+		if (propertyDescriptor.isRequired()) {
+			_checkBox = new JCheckBox();
+			_comboBox = null;
+			_checkBox.setOpaque(false);
+			if (currentValue != null) {
+				_checkBox.setSelected(currentValue.booleanValue());
 			}
-		});
-		add(_checkBox);
+			_checkBox.addChangeListener(new ChangeListener() {
+				@Override
+				public void stateChanged(ChangeEvent e) {
+					fireValueChanged();
+				}
+			});
+			add(_checkBox);
+		} else {
+			_checkBox = null;
+			_comboBox = new JComboBox(new String[] { "true", "false", LabelConstants.NULL_LABEL });
+
+			if (currentValue == null) {
+				_comboBox.setSelectedItem(LabelConstants.NULL_LABEL);
+			} else {
+				_comboBox.setSelectedItem(currentValue.toString());
+			}
+
+			_comboBox.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					fireValueChanged();
+				}
+			});
+
+			add(_comboBox);
+		}
 	}
 
 	@Override
 	public boolean isSet() {
+		if (_comboBox != null) {
+			String selectedItem = (String) _comboBox.getSelectedItem();
+			if (LabelConstants.NULL_LABEL.equals(selectedItem)) {
+				return false;
+			}
+		}
 		return true;
 	}
 
 	@Override
 	public Boolean getValue() {
-		return _checkBox.isSelected();
+		if (_checkBox == null) {
+			String selectedItem = (String) _comboBox.getSelectedItem();
+			if (LabelConstants.NULL_LABEL.equals(selectedItem)) {
+				return null;
+			} else {
+				return Boolean.parseBoolean(selectedItem);
+			}
+		} else {
+			return _checkBox.isSelected();
+		}
 	}
 
 	@Override
 	protected void setValue(Boolean value) {
-		if (value == null) {
-			_checkBox.setSelected(false);
+		if (_checkBox == null) {
+			if (value == null) {
+				_comboBox.setSelectedItem(LabelConstants.NULL_LABEL);
+			} else {
+				if (value.booleanValue()) {
+					_comboBox.setSelectedItem("true");
+				} else {
+					_comboBox.setSelectedItem("false");
+				}
+			}
+		} else {
+			if (value == null) {
+				_checkBox.setSelected(false);
+			} else {
+				_checkBox.setSelected(value.booleanValue());
+			}
 		}
-		_checkBox.setSelected(value.booleanValue());
 	}
 
 }

8ae69c3492392b0dc55d14123feaba2c7f935839
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2011-04-03 09:53:32 +0000

    Updating version in svn to 2.2-SNAPSHOT.

--- src/main/java/org/eobjects/datacleaner/Main.java
+++ src/main/java/org/eobjects/datacleaner/Main.java
@@ -33,7 +33,7 @@ import org.eobjects.datacleaner.windows.AnalysisJobBuilderWindow;
 
 public final class Main {
 
-	public static final String VERSION = "2.1";
+	public static final String VERSION = "2.2-SNAPSHOT";
 
 	public static void main(String[] args) throws UnsupportedLookAndFeelException {
 		// set up default error handling

29e65cb3a5b25ee16480aa35bb8194d337e75d2c
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2011-05-03 13:29:06 +0000

    Ticket #592: Added CLI handling to datacleaner. Added DataCleaner-console.exe executable.

--- src/main/java/org/eobjects/datacleaner/Main.java
+++ src/main/java/org/eobjects/datacleaner/Main.java
@@ -19,8 +19,13 @@
  */
 package org.eobjects.datacleaner;
 
+import java.awt.SplashScreen;
+import java.io.PrintWriter;
+
 import javax.swing.UnsupportedLookAndFeelException;
 
+import org.eobjects.analyzer.cli.CliArguments;
+import org.eobjects.analyzer.cli.CliRunner;
 import org.eobjects.analyzer.configuration.AnalyzerBeansConfiguration;
 import org.eobjects.datacleaner.regexswap.RegexSwapUserPreferencesHandler;
 import org.eobjects.datacleaner.user.DCConfiguration;
@@ -30,35 +35,70 @@ import org.eobjects.datacleaner.user.UserPreferences;
 import org.eobjects.datacleaner.util.DCUncaughtExceptionHandler;
 import org.eobjects.datacleaner.util.LookAndFeelManager;
 import org.eobjects.datacleaner.windows.AnalysisJobBuilderWindow;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class Main {
+
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
 	public static final String VERSION = "2.2-SNAPSHOT";
 
 	public static void main(String[] args) throws UnsupportedLookAndFeelException {
-		// set up default error handling
+		logger.info("Welcome to DataCleaner {}", VERSION);
 
-		final DCUncaughtExceptionHandler uncaughtExceptionHandler = new DCUncaughtExceptionHandler();
-		Thread.setDefaultUncaughtExceptionHandler(uncaughtExceptionHandler);
+		// determine whether to run in command line interface mode
+		final CliArguments arguments = CliArguments.parse(args);
+		final boolean cliMode = arguments.isSet();
 
-		// init the look and feel
-		LookAndFeelManager.getInstance().init();
+		logger.info("CLI mode={}, use -usage to view usage options", cliMode);
+
+		if (cliMode) {
+			// hide splash screen
+			SplashScreen.getSplashScreen().close();
+
+			if (arguments.isUsageMode()) {
+				final PrintWriter out = new PrintWriter(System.out);
+				CliArguments.printUsage(out);
+
+				System.exit(1);
+			}
+		} else {
+			// set up error handling that displays an error dialog
+			final DCUncaughtExceptionHandler uncaughtExceptionHandler = new DCUncaughtExceptionHandler();
+			Thread.setDefaultUncaughtExceptionHandler(uncaughtExceptionHandler);
+
+			// init the look and feel
+			LookAndFeelManager.getInstance().init();
+		}
 
 		// loads static configuration
 		final AnalyzerBeansConfiguration configuration = DCConfiguration.get();
 
-		// loads dynamic user preferences
-		final UserPreferences userPreferences = UserPreferences.getInstance();
-
-		new AnalysisJobBuilderWindow(configuration).setVisible(true);
-
 		// log usage
 		UsageLogger.getInstance().logApplicationStartup();
 
-		// load regex swap regexes if logged in
-		final RegexSwapUserPreferencesHandler regexSwapHandler = new RegexSwapUserPreferencesHandler(
-				(MutableReferenceDataCatalog) configuration.getReferenceDataCatalog());
-		userPreferences.addLoginChangeListener(regexSwapHandler);
+		if (cliMode) {
 
+			final PrintWriter out = new PrintWriter(System.out);
+			// run in CLI mode
+			final CliRunner runner = new CliRunner(arguments, out);
+			runner.run(configuration);
+			out.flush();
+
+			System.exit(0);
+		} else {
+			// run in GUI mode
+
+			// loads dynamic user preferences
+			final UserPreferences userPreferences = UserPreferences.getInstance();
+
+			new AnalysisJobBuilderWindow(configuration).setVisible(true);
+
+			// load regex swap regexes if logged in
+			final RegexSwapUserPreferencesHandler regexSwapHandler = new RegexSwapUserPreferencesHandler(
+					(MutableReferenceDataCatalog) configuration.getReferenceDataCatalog());
+			userPreferences.addLoginChangeListener(regexSwapHandler);
+		}
 	}
 }

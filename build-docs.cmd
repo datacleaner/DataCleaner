b23acdf9124f29782f9a33599d54734f553ce5e5
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2011-05-16 07:37:23 +0000

    Minor additions to docs

--- build-docs.cmd
+++ build-docs.cmd
@@ -1,3 +1,4 @@
 @echo off
 set MAVEN_OPTS=-Dorg.apache.xerces.xni.parser.XMLParserConfiguration=org.apache.xerces.parsers.XIncludeParserConfiguration
+cd \dev\eclipse-analyzerbeans\workspace\DataCleaner
 mvn generate-resources xml:transform

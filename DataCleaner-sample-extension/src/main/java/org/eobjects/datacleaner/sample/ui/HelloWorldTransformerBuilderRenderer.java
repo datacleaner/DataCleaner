11789802b32b2b148d1f6fdfef3bcb8491d33188
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2011-06-15 09:47:53 +0000

    Added sample UI modifications in extension.

---
+++ DataCleaner-sample-extension/src/main/java/org/eobjects/datacleaner/sample/ui/HelloWorldTransformerBuilderRenderer.java
@@ -0,0 +1,61 @@
+/**
+ * eobjects.org DataCleaner
+ * Copyright (C) 2010 eobjects.org
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.eobjects.datacleaner.sample.ui;
+
+import javax.inject.Inject;
+
+import org.eobjects.analyzer.beans.api.Renderer;
+import org.eobjects.analyzer.beans.api.RendererBean;
+import org.eobjects.analyzer.beans.api.RendererPrecedence;
+import org.eobjects.analyzer.job.builder.TransformerJobBuilder;
+import org.eobjects.datacleaner.bootstrap.WindowManager;
+import org.eobjects.datacleaner.panels.ComponentJobBuilderRenderingFormat;
+import org.eobjects.datacleaner.panels.TransformerJobBuilderPresenter;
+import org.eobjects.datacleaner.sample.HelloWorldTransformer;
+
+/**
+ * A sample renderer which provides a presenter object for the configuration
+ * panel of a transformer.
+ * 
+ * Renderers like this are optional, but allows for absolute control over the
+ * User Interface which may be useful for certain types of extensions.
+ * 
+ * @author Kasper Sørensen
+ */
+@RendererBean(ComponentJobBuilderRenderingFormat.class)
+public class HelloWorldTransformerBuilderRenderer implements
+		Renderer<TransformerJobBuilder<?>, TransformerJobBuilderPresenter> {
+
+	@Inject
+	WindowManager windowManager;
+
+	@Override
+	public RendererPrecedence getPrecedence(TransformerJobBuilder<?> renderable) {
+		if (renderable.getDescriptor().getComponentClass() == HelloWorldTransformer.class) {
+			return RendererPrecedence.HIGHEST;
+		}
+		return RendererPrecedence.NOT_CAPABLE;
+	}
+
+	@Override
+	public TransformerJobBuilderPresenter render(TransformerJobBuilder<?> tjb) {
+		return new HelloWorldTransformerPresenter(tjb, windowManager);
+	}
+}
---
+++ DataCleaner-sample-extension/src/main/java/org/eobjects/datacleaner/sample/ui/HelloWorldTransformerPresenter.java
@@ -0,0 +1,56 @@
+/**
+ * eobjects.org DataCleaner
+ * Copyright (C) 2010 eobjects.org
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.eobjects.datacleaner.sample.ui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import org.eobjects.analyzer.job.builder.TransformerJobBuilder;
+import org.eobjects.datacleaner.bootstrap.WindowManager;
+import org.eobjects.datacleaner.panels.TransformerJobBuilderPanel;
+import org.eobjects.datacleaner.panels.TransformerJobBuilderPresenter;
+
+public class HelloWorldTransformerPresenter extends TransformerJobBuilderPanel implements TransformerJobBuilderPresenter {
+
+	private static final long serialVersionUID = 1L;
+
+	public HelloWorldTransformerPresenter(TransformerJobBuilder<?> transformerJobBuilder, WindowManager windowManager) {
+		super(transformerJobBuilder, windowManager);
+	}
+
+	@Override
+	public JComponent createJComponent() {
+		// delegate to the parent implementation
+		JComponent parentComponent = super.createJComponent();
+
+		JPanel panel = new JPanel(new BorderLayout());
+		JLabel label = new JLabel("Hello DataCleaner users! This label was drawn by our extension!");
+		label.setOpaque(false);
+		label.setBorder(new EmptyBorder(20, 20, 20, 20));
+
+		panel.add(label, BorderLayout.NORTH);
+		panel.add(parentComponent, BorderLayout.CENTER);
+		return panel;
+	}
+}

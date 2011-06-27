ad48cbc79dac648a1fdc79b1348aa7bfb2ccebea
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2011-06-27 07:02:59 +0000

    Preparing release: Removing snapshot from version numbers

--- DataCleaner-core/src/main/java/org/eobjects/datacleaner/Main.java
+++ DataCleaner-core/src/main/java/org/eobjects/datacleaner/Main.java
@@ -25,7 +25,7 @@ import org.eobjects.datacleaner.bootstrap.DefaultBootstrapOptions;
 
 public final class Main {
 
-	public static final String VERSION = "2.2-SNAPSHOT";
+	public static final String VERSION = "2.2";
 
 	public static void main(String[] args) {
 		BootstrapOptions bootstrapOptions = new DefaultBootstrapOptions(args);

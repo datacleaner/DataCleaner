97e13747601bc07377ba19ff1b9cec3f4c608264
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2011-09-29 08:21:35 +0000

    Preparing for 2.3 release

--- core/src/main/java/org/eobjects/datacleaner/Main.java
+++ core/src/main/java/org/eobjects/datacleaner/Main.java
@@ -37,7 +37,7 @@ import org.eobjects.datacleaner.user.DataCleanerHome;
  */
 public final class Main {
 
-	public static final String VERSION = "2.3-SNAPSHOT";
+	public static final String VERSION = "2.3";
 
 	public static void main(String[] args) {
 		initializeLogging();

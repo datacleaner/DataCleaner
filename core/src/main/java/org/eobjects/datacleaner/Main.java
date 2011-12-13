6735b0518cad809cd102a391b26c35e9f66b42cc
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2011-12-13 14:01:42 +0000

    Changed code-version to 2.5-SNAPSHOT

--- core/src/main/java/org/eobjects/datacleaner/Main.java
+++ core/src/main/java/org/eobjects/datacleaner/Main.java
@@ -37,7 +37,7 @@ import org.eobjects.datacleaner.user.DataCleanerHome;
  */
 public final class Main {
 
-	public static final String VERSION = "2.4";
+	public static final String VERSION = "2.5-SNAPSHOT";
 
 	public static void main(String[] args) {
 		initializeLogging();

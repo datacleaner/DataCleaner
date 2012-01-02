cca09af1e91400bbb0c4972de353750fb6a71443
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2012-01-02 10:16:30 +0000

    Ticket #737: Added RSyntaxTextArea for mimetypes supported by it

--- core/src/main/java/org/eobjects/datacleaner/widgets/properties/SingleStringPropertyWidget.java
+++ core/src/main/java/org/eobjects/datacleaner/widgets/properties/SingleStringPropertyWidget.java
@@ -19,6 +19,11 @@
  */
 package org.eobjects.datacleaner.widgets.properties;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.swing.event.DocumentEvent;
 import javax.swing.text.JTextComponent;
@@ -28,12 +33,13 @@ import org.eobjects.analyzer.descriptors.ConfiguredPropertyDescriptor;
 import org.eobjects.analyzer.job.builder.AbstractBeanJobBuilder;
 import org.eobjects.datacleaner.util.DCDocumentListener;
 import org.eobjects.datacleaner.util.WidgetFactory;
-import org.eobjects.datacleaner.util.WidgetUtils;
+import org.eobjects.metamodel.util.CollectionUtils;
+import org.eobjects.metamodel.util.Func;
+import org.eobjects.metamodel.util.Predicate;
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
 
 public class SingleStringPropertyWidget extends AbstractPropertyWidget<String> {
-
-	private static final String[] MONOSPACE_MIME_TYPES = { "text/x-java-source", "application/x-javascript",
-			"text/javascript" };
 
 	private final JTextComponent _textComponent;
 
@@ -53,9 +59,27 @@ public class SingleStringPropertyWidget extends AbstractPropertyWidget<String> {
 
 	private JTextComponent getTextComponent(ConfiguredPropertyDescriptor propertyDescriptor,
 			StringProperty stringPropertyAnnotation) {
-		JTextComponent textComponent;
-		if (stringPropertyAnnotation != null && stringPropertyAnnotation.multiline()) {
-			textComponent = WidgetFactory.createTextArea(propertyDescriptor.getName());
+		final boolean multiline;
+		final String mimeType;
+
+		if (stringPropertyAnnotation == null) {
+			multiline = false;
+			mimeType = null;
+		} else {
+			multiline = stringPropertyAnnotation.multiline();
+			String[] mimeTypes = stringPropertyAnnotation.mimeType();
+			mimeType = getTextAreaMimeType(mimeTypes);
+		}
+
+		final JTextComponent textComponent;
+		if (multiline) {
+			if (mimeType != null) {
+				RSyntaxTextArea syntaxArea = new RSyntaxTextArea(8, 17);
+				syntaxArea.setSyntaxEditingStyle(mimeType);
+				textComponent = syntaxArea;
+			} else {
+				textComponent = WidgetFactory.createTextArea(propertyDescriptor.getName());
+			}
 		} else {
 			textComponent = WidgetFactory.createTextField(propertyDescriptor.getName());
 		}
@@ -67,24 +91,54 @@ public class SingleStringPropertyWidget extends AbstractPropertyWidget<String> {
 			}
 		});
 
-		if (stringPropertyAnnotation != null) {
-			boolean monospace = false;
-			String[] mimeTypes = stringPropertyAnnotation.mimeType();
-			for (String mt1 : mimeTypes) {
-				for (String mt2 : MONOSPACE_MIME_TYPES) {
-					if (mt1.equalsIgnoreCase(mt2)) {
-						monospace = true;
-						break;
+		return textComponent;
+	}
+
+	/**
+	 * Finds a mimetype that is supported by the {@link RSyntaxTextArea}. These
+	 * are defined in the {@link SyntaxConstants} class.
+	 * 
+	 * @param mimeTypes
+	 * @return
+	 */
+	private String getTextAreaMimeType(String[] mimeTypes) {
+		if (mimeTypes == null) {
+			return null;
+		}
+
+		List<Field> fields = Arrays.asList(SyntaxConstants.class.getFields());
+		fields = CollectionUtils.filter(fields, new Predicate<Field>() {
+			@Override
+			public Boolean eval(Field f) {
+				if (f.getName().startsWith("SYNTAX_STYLE_")) {
+					if (f.getType() == String.class) {
+						final int modifiers = f.getModifiers();
+						final boolean accessible = f.isAccessible() || Modifier.isPublic(modifiers);
+						final boolean isStatic = Modifier.isStatic(modifiers);
+						return accessible && isStatic;
 					}
 				}
+				return false;
 			}
+		});
+		List<String> acceptedMimeTypes = CollectionUtils.map(fields, new Func<Field, String>() {
+			@Override
+			public String eval(Field f) {
+				try {
+					return (String) f.get(null);
+				} catch (Exception e) {
+					return null;
+				}
+			}
+		});
 
-			if (monospace) {
-				textComponent.setFont(WidgetUtils.FONT_MONOSPACE);
+		for (String mimeType : mimeTypes) {
+			if (acceptedMimeTypes.contains(mimeType)) {
+				return mimeType;
 			}
 		}
 
-		return textComponent;
+		return null;
 	}
 
 	@Override

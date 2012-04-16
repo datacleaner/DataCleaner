e4f312bdfd7ba1c7d5a25debd1df52f1bf45ca3d
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2012-04-16 14:21:42 +0000

    Ticket #820: Improved DC shell startup scripts

--- packaging/src/main/assembly/datacleaner.sh
+++ packaging/src/main/assembly/datacleaner.sh
@@ -19,19 +19,23 @@
 # Boston, MA  02110-1301  USA
 
 cd `dirname $0`
-DATACLEANER_HOME=`pwd`
-export DATACLEANER_HOME
+export DATACLEANER_HOME=`pwd`
 echo "Using DATACLEANER_HOME: $DATACLEANER_HOME"
 
-JAVA_OPTS="$JAVA_OPTS -Xmx1024m"
+export DATACLEANER_LIB_HOME=$DATACLEANER_HOME
+echo "Using DATACLEANER_LIB_HOME: $DATACLEANER_LIB_HOME"
+
+mkdir -p $DATACLEANER_HOME
+cd $DATACLEANER_HOME
 
 macos=false
 case "`uname`" in
 	Darwin*) macos=true;;
 esac
 
+DATACLEANER_JAVA_OPTS="$JAVA_OPTS -Xmx1024m"
 if $macos; then
-	JAVA_OPTS="$JAVA_OPTS -Xdock:name=DataCleaner"
+	DATACLEANER_JAVA_OPTS="$DATACLEANER_JAVA_OPTS -Xdock:name=DataCleaner"
 fi
 
-exec java $JAVA_OPTS -jar DataCleaner.jar $*
+exec java $DATACLEANER_JAVA_OPTS -jar $DATACLEANER_LIB_HOME/DataCleaner.jar $*

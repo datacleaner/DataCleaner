e945668876668ffb8987b1f553840d6a6a9172a2
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2012-06-07 13:11:32 +0000

    Fixed unittest

--- monitor/src/test/java/org/eobjects/datacleaner/monitor/server/TimelineServiceImplTest.java
+++ monitor/src/test/java/org/eobjects/datacleaner/monitor/server/TimelineServiceImplTest.java
@@ -33,6 +33,7 @@ import org.eobjects.datacleaner.monitor.timeline.model.TenantIdentifier;
 import org.eobjects.datacleaner.monitor.timeline.model.TimelineData;
 import org.eobjects.datacleaner.monitor.timeline.model.TimelineDataRow;
 import org.eobjects.datacleaner.monitor.timeline.model.TimelineDefinition;
+import org.eobjects.datacleaner.monitor.timeline.model.TimelineGroup;
 import org.eobjects.datacleaner.monitor.timeline.model.TimelineIdentifier;
 import org.eobjects.datacleaner.repository.file.FileRepository;
 
@@ -50,53 +51,56 @@ public class TimelineServiceImplTest extends TestCase {
         assertEquals(1, jobs.size());
 
         final JobIdentifier job = jobs.get(0);
-        assertEquals("JobIdentifier[name=customers,path=/DC/jobs/customers.analysis.xml]", job.toString());
+        assertEquals("JobIdentifier[name=product_profiling,path=/DC/jobs/product_profiling.analysis.xml]",
+                job.toString());
 
         final JobMetrics jobMetrics = service.getJobMetrics(tenant, job);
-        assertEquals("JobMetrics[customers metrics]", jobMetrics.toString());
+        assertEquals("JobMetrics[product_profiling metrics]", jobMetrics.toString());
         assertSame(job, jobMetrics.getJob());
 
         final List<MetricGroup> metricGroups = jobMetrics.getMetricGroups();
-        assertEquals(4, metricGroups.size());
+        assertEquals(3, metricGroups.size());
 
-        assertEquals("MetricGroup[Pattern finder (CONTACTLASTNAME)]", metricGroups.get(0).toString());
-        assertEquals("MetricGroup[Pattern finder (CONTACTFIRSTNAME)]", metricGroups.get(1).toString());
-        assertEquals("MetricGroup[Value distribution (CITY)]", metricGroups.get(2).toString());
-        assertEquals("MetricGroup[String analyzer (CITY,CONTACTLASTNAME,CONTACTFIRSTNAME)]", metricGroups.get(3).toString());
+        assertEquals("MetricGroup[Pattern finder (PRODUCTCODE)]", metricGroups.get(0).toString());
+        assertEquals("MetricGroup[Value distribution (PRODUCTLINE)]", metricGroups.get(1).toString());
+        assertEquals("MetricGroup[Vendor whitelist check (Reference data matcher) (PRODUCTVENDOR)]", metricGroups
+                .get(2).toString());
 
         List<MetricIdentifier> metrics = metricGroups.get(2).getMetrics();
-        assertEquals("[MetricIdentifier[analyzerInputName=CITY,metricDescriptorName=Distinct count], MetricIdentifier[analyzerInputName=CITY,metricDescriptorName=Null count], MetricIdentifier[analyzerInputName=CITY,metricDescriptorName=Total count], MetricIdentifier[analyzerInputName=CITY,metricDescriptorName=Unique count], MetricIdentifier[analyzerInputName=CITY,metricDescriptorName=Value count]]", metrics.toString());
-        
-        metrics = metricGroups.get(3).getMetrics();
-        // 20 metrics in the String analyzer
-        assertEquals(20, metrics.size());
-        assertEquals(null, metrics.get(0).getAnalyzerInputName());
-        assertEquals("Avg chars", metrics.get(0).getMetricDescriptorName());
+        assertEquals(
+                "[MetricIdentifier[analyzerInputName=PRODUCTVENDOR,metricDescriptorName=Row count], MetricIdentifier[analyzerInputName=PRODUCTVENDOR,metricDescriptorName=Null count], MetricIdentifier[analyzerInputName=PRODUCTVENDOR,metricDescriptorName=True count], MetricIdentifier[analyzerInputName=PRODUCTVENDOR,metricDescriptorName=False count], MetricIdentifier[analyzerInputName=PRODUCTVENDOR,metricDescriptorName=Total combination count], MetricIdentifier[analyzerInputName=PRODUCTVENDOR,metricDescriptorName=Combination count]]",
+                metrics.toString());
 
-        final List<TimelineIdentifier> timelines = service.getTimelines(tenant, null);
-        assertEquals(1, timelines.size());
+        metrics = metricGroups.get(0).getMetrics();
+        // 2 metrics in the Pattern finder
+        assertEquals(2, metrics.size());
+        assertEquals("PRODUCTCODE", metrics.get(0).getAnalyzerInputName());
+        assertEquals("Match count", metrics.get(0).getMetricDescriptorName());
+
+        List<TimelineIdentifier> timelines = service.getTimelines(tenant, null);
+        assertEquals(0, timelines.size());
+
+        final List<TimelineGroup> timelineGroups = service.getTimelineGroups(tenant);
+        assertEquals(1, timelineGroups.size());
+        
+        timelines = service.getTimelines(tenant, timelineGroups.get(0));
+        assertEquals(3, timelines.size());
 
         final TimelineIdentifier timelineIdentifier = timelines.get(0);
         assertEquals(
-                "TimelineIdentifier[name=customer_patterns,path=/DC/timelines/customer_patterns.analysis.timeline.xml]",
+                "TimelineIdentifier[name=Product code patterns (the lower the better),path=/DC/timelines/Product data/Product code patterns (the lower the better).analysis.timeline.xml]",
                 timelineIdentifier.toString());
 
         final TimelineDefinition timelineDefinition = service.getTimelineDefinition(tenant, timelineIdentifier);
-        assertEquals("TimelineDefinition[job=JobIdentifier[name=null,path=/DC/jobs/customers.analysis.xml],metrics=["
-                + "MetricIdentifier[analyzerInputName=CONTACTLASTNAME,metricDescriptorName=Pattern count], "
-                + "MetricIdentifier[analyzerInputName=CITY,metricDescriptorName=Value count,paramQueryString=Madrid], "
-                + "MetricIdentifier[analyzerInputName=CITY,metricDescriptorName=Value count,paramQueryString=NYC], "
-                + "MetricIdentifier[analyzerInputName=null,metricDescriptorName=Digit chars,paramColumnName=CITY]]]",
+        assertEquals("TimelineDefinition[job=JobIdentifier[name=null,path=/DC/jobs/product_profiling.analysis.xml],metrics=[MetricIdentifier[analyzerInputName=PRODUCTCODE,metricDescriptorName=Pattern count], MetricIdentifier[analyzerInputName=PRODUCTVENDOR,metricDescriptorName=False count,paramQueryString=PRODUCTVENDOR in 'vendor whitelist']]]",
                 timelineDefinition.toString());
-        
+
         final TimelineData timelineData = service.getTimelineData(tenant, timelineDefinition);
-        assertEquals("TimelineData[3 rows]", timelineData.toString());
-        
+        assertEquals("TimelineData[6 rows]", timelineData.toString());
+
         final List<TimelineDataRow> rows = timelineData.getRows();
-        assertEquals(3, rows.size());
-        
-        assertEquals("TimelineDataRow[date=2012-5-7 21:12,metricValues=[8, 6, 5, 8]]", rows.get(0).toString());
-        assertEquals("TimelineDataRow[date=2012-5-8 16:15,metricValues=[8, 6, 5, 8]]", rows.get(1).toString());
-        assertEquals("TimelineDataRow[date=2012-5-8 23:12,metricValues=[8, 0, 133, 0]]", rows.get(2).toString());
+        assertEquals(6, rows.size());
+
+        assertEquals("TimelineDataRow[date=2012-6-6 15:46,metricValues=[5, 0]]", rows.get(0).toString());
     }
 }

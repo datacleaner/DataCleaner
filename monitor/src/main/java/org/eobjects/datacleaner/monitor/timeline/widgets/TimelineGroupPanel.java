c9017762c2d8bf1961279c0d60fc8bc6c643adba
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2012-07-02 09:29:03 +0000

    Ticket #837: Added button for removing timeline group.

--- monitor/src/main/java/org/eobjects/datacleaner/monitor/timeline/widgets/TimelineGroupPanel.java
+++ monitor/src/main/java/org/eobjects/datacleaner/monitor/timeline/widgets/TimelineGroupPanel.java
@@ -28,6 +28,9 @@ import org.eobjects.datacleaner.monitor.timeline.model.TimelineGroup;
 import org.eobjects.datacleaner.monitor.timeline.model.TimelineIdentifier;
 import org.eobjects.datacleaner.monitor.util.DCAsyncCallback;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
@@ -41,33 +44,64 @@ public class TimelineGroupPanel extends FlowPanel {
     private final TenantIdentifier _tenant;
     private final TimelineServiceAsync _service;
     private final WelcomePanel _welcomePanel;
-    private TimelineGroup _group;
+    private final TimelineGroup _group;
+    private final Button _removeGroupButton;
+    private int _dashboardWidgetCount;
 
     public TimelineGroupPanel(TimelineServiceAsync service, TenantIdentifier tenant, TimelineGroup group) {
         super();
         _tenant = tenant;
         _service = service;
         _group = group;
+        _dashboardWidgetCount = 0;
 
         addStyleName("TimelineGroupPanel");
 
+        _removeGroupButton = new Button("Remove this group");
+        _removeGroupButton.addStyleDependentName("ImageButton");
+        _removeGroupButton.addStyleName("RemoveButton");
+        _removeGroupButton.setVisible(false);
+        _removeGroupButton.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                final boolean confirmation = Window.confirm("Are you sure you wish to remove this group?");
+                if (confirmation) {
+                    _service.removeTimelineGroup(_tenant, _group, new DCAsyncCallback<Boolean>() {
+                        @Override
+                        public void onSuccess(Boolean result) {
+                            if (result != null && result.booleanValue()) {
+                                Window.Location.reload();
+                            } else {
+                                Window.alert("Failed to remove group. Please check server logs for details.");
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
         final Button newTimelineButton;
         if (group == null) {
+            // this is the "default" group
             add(new HeadingLabel("Welcome"));
             _welcomePanel = new WelcomePanel();
             newTimelineButton = _welcomePanel.getNewTimelineButton();
             add(_welcomePanel);
         } else {
             add(new HeadingLabel(group.getName()));
-            
+
             if (group.getDescription() != null) {
                 add(new Label(group.getDescription()));
             }
-            
+
             _welcomePanel = null;
             newTimelineButton = new Button("New timeline");
             add(newTimelineButton);
+            add(_removeGroupButton);
         }
+        
+        newTimelineButton.addStyleDependentName("ImageButton");
+        newTimelineButton.addStyleName("NewDashboardWidgetButton");
         newTimelineButton.addClickHandler(new CreateTimelineHandler(_service, _tenant, this));
 
         _service.getTimelines(_tenant, _group, new DCAsyncCallback<List<TimelineIdentifier>>() {
@@ -75,6 +109,10 @@ public class TimelineGroupPanel extends FlowPanel {
             public void onSuccess(List<TimelineIdentifier> result) {
                 for (TimelineIdentifier identifier : result) {
                     addTimelinePanel(identifier);
+                }
+
+                if (_dashboardWidgetCount == 0) {
+                    _removeGroupButton.setVisible(true);
                 }
             }
         });
@@ -90,10 +128,16 @@ public class TimelineGroupPanel extends FlowPanel {
         if (_welcomePanel != null) {
             _welcomePanel.setWelcomeTextVisible(false);
         }
+        _dashboardWidgetCount++;
+        _removeGroupButton.setVisible(false);
     }
 
     public void removeTimelinePanel(TimelinePanel timelinePanel) {
         remove(timelinePanel);
+        _dashboardWidgetCount--;
+        if (_dashboardWidgetCount == 0) {
+            _removeGroupButton.setVisible(true);
+        }
     }
 
     public TimelineGroup getTimelineGroup() {

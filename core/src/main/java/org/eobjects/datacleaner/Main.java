fcca1ad5434a8e091a00e6ce12f4afb0d5a8b1b1
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2012-08-17 10:52:39 +0000

    Preparing release of 3.0-alpha

--- core/src/main/java/org/eobjects/datacleaner/Main.java
+++ core/src/main/java/org/eobjects/datacleaner/Main.java
@@ -45,7 +45,7 @@ import org.eobjects.datacleaner.user.DataCleanerHome;
  */
 public final class Main {
 
-    public static final String VERSION = "3.0-SNAPSHOT";
+    public static final String VERSION = "3.0 (alpha)";
 
     public static void main(String[] args) {
         initializeSystemProperties(args);

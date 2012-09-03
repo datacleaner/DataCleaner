881921f838cc952fef4208434225fbe32fc62be7
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2012-09-03 11:46:21 +0000

    Made it possible to externalize html rendering artifacts (in repository-context.xml).

---
+++ monitor/services/src/main/java/org/eobjects/datacleaner/monitor/server/HtmlAnalysisResultWriterFactory.java
@@ -0,0 +1,61 @@
+/**
+ * eobjects.org DataCleaner
+ * Copyright (C) 2010 eobjects.org
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.eobjects.datacleaner.monitor.server;
+
+import java.util.Map.Entry;
+
+import org.eobjects.analyzer.job.ComponentJob;
+import org.eobjects.analyzer.result.AnalyzerResult;
+import org.eobjects.analyzer.result.html.BaseHeadElement;
+import org.eobjects.analyzer.result.html.HeadElement;
+import org.eobjects.analyzer.result.html.HtmlAnalysisResultWriter;
+import org.eobjects.metamodel.util.Predicate;
+import org.springframework.stereotype.Component;
+
+/**
+ * Factory for {@link HtmlAnalysisResultWriter} objects.
+ */
+@Component
+public class HtmlAnalysisResultWriterFactory {
+
+    private String resourcesDirectory;
+
+    public String getResourcesDirectory() {
+        return resourcesDirectory;
+    }
+
+    public void setResourcesDirectory(String resourcesDirectory) {
+        this.resourcesDirectory = resourcesDirectory;
+    }
+
+    public HtmlAnalysisResultWriter create(boolean tabs,
+            Predicate<Entry<ComponentJob, AnalyzerResult>> jobInclusionPredicate, boolean headers) {
+        return new HtmlAnalysisResultWriter(tabs, jobInclusionPredicate, headers) {
+            @Override
+            protected HeadElement createBaseHeadElement() {
+                if (resourcesDirectory == null) {
+                    return super.createBaseHeadElement();
+                } else {
+                    return new BaseHeadElement(resourcesDirectory);
+                }
+            }
+        };
+    }
+}
--- monitor/services/src/main/java/org/eobjects/datacleaner/monitor/server/controllers/ResultFileController.java
+++ monitor/services/src/main/java/org/eobjects/datacleaner/monitor/server/controllers/ResultFileController.java
@@ -37,6 +37,7 @@ import org.eobjects.analyzer.result.AnalyzerResult;
 import org.eobjects.analyzer.result.html.HtmlAnalysisResultWriter;
 import org.eobjects.datacleaner.monitor.configuration.TenantContext;
 import org.eobjects.datacleaner.monitor.configuration.TenantContextFactory;
+import org.eobjects.datacleaner.monitor.server.HtmlAnalysisResultWriterFactory;
 import org.eobjects.datacleaner.monitor.shared.model.SecurityRoles;
 import org.eobjects.datacleaner.repository.RepositoryFile;
 import org.eobjects.datacleaner.repository.RepositoryFolder;
@@ -60,131 +61,132 @@ public class ResultFileController {
 
     private static final String EXTENSION = FileFilters.ANALYSIS_RESULT_SER.getExtension();
 
-	@Autowired
-	TenantContextFactory _contextFactory;
+    @Autowired
+    TenantContextFactory _contextFactory;
 
-	@RolesAllowed({ SecurityRoles.JOB_EDITOR, SecurityRoles.SCHEDULE_EDITOR })
-	@RequestMapping(method = RequestMethod.POST, produces = "application/json")
-	@ResponseBody
+    @Autowired
+    HtmlAnalysisResultWriterFactory _htmlAnalysisResultWriterFactory;
+
+    @RolesAllowed({ SecurityRoles.JOB_EDITOR, SecurityRoles.SCHEDULE_EDITOR })
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    @ResponseBody
     public Map<String, String> uploadAnalysisResult(@PathVariable("tenant") String tenant,
             @PathVariable("result") String resultName, @RequestParam("file") final MultipartFile file) {
-		if (file == null) {
-			throw new IllegalArgumentException(
-					"No file upload provided. Please provide a multipart file using the 'file' HTTP parameter.");
-		}
+        if (file == null) {
+            throw new IllegalArgumentException(
+                    "No file upload provided. Please provide a multipart file using the 'file' HTTP parameter.");
+        }
 
-		resultName = resultName.replaceAll("\\+", " ");
+        resultName = resultName.replaceAll("\\+", " ");
 
-		final TenantContext context = _contextFactory.getContext(tenant);
+        final TenantContext context = _contextFactory.getContext(tenant);
 
-		final RepositoryFolder resultsFolder = context.getResultFolder();
+        final RepositoryFolder resultsFolder = context.getResultFolder();
 
-		final long timestamp = new Date().getTime();
-		final String filename;
-		if (resultName.endsWith(EXTENSION)) {
+        final long timestamp = new Date().getTime();
+        final String filename;
+        if (resultName.endsWith(EXTENSION)) {
             filename = resultName.substring(0, resultName.length() - EXTENSION.length()) + "-" + timestamp + EXTENSION;
-		} else {
-			filename = resultName + "-" + timestamp + EXTENSION;
-		}
+        } else {
+            filename = resultName + "-" + timestamp + EXTENSION;
+        }
 
         final RepositoryFile resultFile = resultsFolder.createFile(filename, new Action<OutputStream>() {
-					@Override
-					public void run(OutputStream out) throws Exception {
-						final InputStream in = file.getInputStream();
-						try {
-							FileHelper.copy(in, out);
-						} finally {
-							FileHelper.safeClose(in);
-						}
-					}
-				});
+            @Override
+            public void run(OutputStream out) throws Exception {
+                final InputStream in = file.getInputStream();
+                try {
+                    FileHelper.copy(in, out);
+                } finally {
+                    FileHelper.safeClose(in);
+                }
+            }
+        });
 
-		final Map<String, String> result = new HashMap<String, String>();
-		result.put("status", "Success");
-		result.put("file_type", resultFile.getType().toString());
-		result.put("filename", resultFile.getName());
-		result.put("repository_path", resultFile.getQualifiedPath());
+        final Map<String, String> result = new HashMap<String, String>();
+        result.put("status", "Success");
+        result.put("file_type", resultFile.getType().toString());
+        result.put("filename", resultFile.getName());
+        result.put("repository_path", resultFile.getQualifiedPath());
 
-		return result;
-	}
+        return result;
+    }
 
-	@RolesAllowed(SecurityRoles.VIEWER)
-	@RequestMapping(method = RequestMethod.GET, produces = "text/html")
+    @RolesAllowed(SecurityRoles.VIEWER)
+    @RequestMapping(method = RequestMethod.GET, produces = "text/html")
     public void resultHtml(@PathVariable("tenant") final String tenant, @PathVariable("result") String resultName,
-			@RequestParam(value = "tabs", required = false) Boolean tabsParam,
-			@RequestParam(value = "comp_name", required = false) String componentParamName,
+            @RequestParam(value = "tabs", required = false) Boolean tabsParam,
+            @RequestParam(value = "comp_name", required = false) String componentParamName,
             @RequestParam(value = "comp_index", required = false) Integer componentIndexParam, final Writer out) {
 
-		resultName = resultName.replaceAll("\\+", " ");
+        resultName = resultName.replaceAll("\\+", " ");
 
-		final TenantContext context = _contextFactory.getContext(tenant);
+        final TenantContext context = _contextFactory.getContext(tenant);
 
-		final RepositoryFolder resultsFolder = context.getResultFolder();
+        final RepositoryFolder resultsFolder = context.getResultFolder();
 
-		if (!resultName.endsWith(EXTENSION)) {
-			resultName = resultName + EXTENSION;
-		}
+        if (!resultName.endsWith(EXTENSION)) {
+            resultName = resultName + EXTENSION;
+        }
 
-		final RepositoryFile resultFile = resultsFolder.getFile(resultName);
-		if (resultFile == null) {
+        final RepositoryFile resultFile = resultsFolder.getFile(resultName);
+        if (resultFile == null) {
             throw new IllegalArgumentException("No such result file: " + resultName);
-		}
+        }
 
-		final AnalysisResult analysisResult = context.getResult(resultName)
-				.getAnalysisResult();
-		final AnalyzerBeansConfiguration configuration = context
-				.getConfiguration();
-		final boolean tabs = (tabsParam == null ? true : tabsParam
-				.booleanValue());
+        final AnalysisResult analysisResult = context.getResult(resultName).getAnalysisResult();
+        final AnalyzerBeansConfiguration configuration = context.getConfiguration();
+        final boolean tabs = (tabsParam == null ? true : tabsParam.booleanValue());
 
-		final boolean headers;
-		final Predicate<Entry<ComponentJob, AnalyzerResult>> jobInclusionPredicate;
+        final boolean headers;
+        final Predicate<Entry<ComponentJob, AnalyzerResult>> jobInclusionPredicate;
         if (org.eobjects.analyzer.util.StringUtils.isNullOrEmpty(componentParamName)) {
-			jobInclusionPredicate = new TruePredicate<Entry<ComponentJob, AnalyzerResult>>();
-			headers = true;
-		} else {
+            jobInclusionPredicate = new TruePredicate<Entry<ComponentJob, AnalyzerResult>>();
+            headers = true;
+        } else {
             jobInclusionPredicate = createInclusionPredicate(componentParamName, componentIndexParam);
-			headers = false;
-		}
+            headers = false;
+        }
 
-        final HtmlAnalysisResultWriter htmlWriter = new HtmlAnalysisResultWriter(tabs, jobInclusionPredicate, headers);
+        final HtmlAnalysisResultWriter htmlWriter = _htmlAnalysisResultWriterFactory.create(tabs,
+                jobInclusionPredicate, headers);
 
-		try {
-			htmlWriter.write(analysisResult, configuration, out);
-		} catch (IOException e) {
-			throw new IllegalStateException(e);
-		}
-	}
+        try {
+            htmlWriter.write(analysisResult, configuration, out);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 
     private Predicate<Entry<ComponentJob, AnalyzerResult>> createInclusionPredicate(final String componentParamName,
             final Integer componentIndexParam) {
-		return new Predicate<Map.Entry<ComponentJob, AnalyzerResult>>() {
+        return new Predicate<Map.Entry<ComponentJob, AnalyzerResult>>() {
 
-			private int index = 0;
+            private int index = 0;
 
-			@Override
-			public Boolean eval(Entry<ComponentJob, AnalyzerResult> entry) {
-				ComponentJob component = entry.getKey();
-				String name = component.getName();
-				if (name != null && name.equals(componentParamName)) {
-					return matchesIndex();
-				}
-				String displayName = component.getDescriptor().getDisplayName();
+            @Override
+            public Boolean eval(Entry<ComponentJob, AnalyzerResult> entry) {
+                ComponentJob component = entry.getKey();
+                String name = component.getName();
+                if (name != null && name.equals(componentParamName)) {
+                    return matchesIndex();
+                }
+                String displayName = component.getDescriptor().getDisplayName();
                 if (displayName != null && displayName.equals(componentParamName)) {
-					return matchesIndex();
-				}
-				return false;
-			}
+                    return matchesIndex();
+                }
+                return false;
+            }
 
-			protected Boolean matchesIndex() {
-				if (componentIndexParam == null) {
-					return true;
-				} else {
-					boolean result = (index == componentIndexParam.intValue());
-					index++;
-					return result;
-				}
-			}
-		};
-	}
+            protected Boolean matchesIndex() {
+                if (componentIndexParam == null) {
+                    return true;
+                } else {
+                    boolean result = (index == componentIndexParam.intValue());
+                    index++;
+                    return result;
+                }
+            }
+        };
+    }
 }

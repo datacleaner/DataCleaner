fcfabc3dbbb2f330a90c7a96f48afb082f6f578a
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2012-10-11 08:38:54 +0000

    Slight improvements to the datastore download option.

---
+++ monitor/services/src/main/java/org/eobjects/datacleaner/monitor/server/controllers/DatastoreDownloadController.java
@@ -0,0 +1,108 @@
+/**
+ * eobjects.org DataCleaner
+ * Copyright (C) 2010 eobjects.org
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.eobjects.datacleaner.monitor.server.controllers;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.annotation.security.RolesAllowed;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eobjects.analyzer.configuration.AnalyzerBeansConfiguration;
+import org.eobjects.analyzer.connection.Datastore;
+import org.eobjects.analyzer.connection.FileDatastore;
+import org.eobjects.datacleaner.monitor.configuration.TenantContextFactory;
+import org.eobjects.datacleaner.monitor.shared.model.SecurityRoles;
+import org.eobjects.metamodel.util.FileHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DatastoreDownloadController {
+
+    private static final Logger logger = LoggerFactory.getLogger(DatastoreDownloadController.class);
+
+    @Autowired
+    TenantContextFactory _tenantContextFactory;
+
+    @RolesAllowed(SecurityRoles.ADMIN)
+    @RequestMapping(value = "/{tenant}/datastores/{datastore}.download", method = RequestMethod.GET)
+    protected void downloadFileRepo(HttpServletRequest request, HttpServletResponse response,
+            @PathVariable("tenant") final String tenant, @PathVariable("datastore") String datastoreName)
+            throws IOException {
+        final AnalyzerBeansConfiguration configuration = _tenantContextFactory.getContext(tenant).getConfiguration();
+
+        datastoreName = datastoreName.replaceAll("\\+", " ");
+        final Datastore ds = configuration.getDatastoreCatalog().getDatastore(datastoreName);
+        if (ds == null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "No such datastore: " + datastoreName);
+            return;
+        }
+
+        if (ds instanceof FileDatastore) {
+            final FileDatastore fileDatastore = (FileDatastore) ds;
+            final String filename = fileDatastore.getFilename();
+            final File file = new File(filename);
+
+            if (!file.exists() || !file.isFile()) {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND, "Datastore file not found: " + filename);
+                return;
+            }
+
+            String username = getUsername();
+
+            logger.info("Serving datastore file {} to user: {}", filename, username);
+
+            final FileInputStream is = new FileInputStream(file);
+            try {
+                response.setHeader("Content-Disposition", "attachment; filename=" + datastoreName);
+
+                final OutputStream os = response.getOutputStream();
+                FileHelper.copy(is, os);
+            } finally {
+                FileHelper.safeClose(is);
+            }
+        } else {
+            response.sendError(HttpServletResponse.SC_NO_CONTENT, "Datastore is not file based: " + datastoreName);
+            return;
+        }
+    }
+
+    private String getUsername() {
+        try {
+            final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            return authentication.getName();
+        } catch (Exception e) {
+            logger.warn("Error occurred retreiving username", e);
+            return null;
+        }
+    }
+
+}

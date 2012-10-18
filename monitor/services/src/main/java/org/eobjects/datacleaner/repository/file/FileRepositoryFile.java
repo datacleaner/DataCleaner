07c4174ed9d00686fd646df441b94dc7a870d045
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2012-10-18 12:34:20 +0000

    Fixed java 7 dependency

--- monitor/services/src/main/java/org/eobjects/datacleaner/repository/file/FileRepositoryFile.java
+++ monitor/services/src/main/java/org/eobjects/datacleaner/repository/file/FileRepositoryFile.java
@@ -25,12 +25,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 import org.eobjects.datacleaner.repository.RepositoryFile;
 import org.eobjects.datacleaner.repository.RepositoryFolder;

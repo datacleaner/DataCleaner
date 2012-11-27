e6d830b2d3d3d20d4ef88f61128794db04b3a9b1
Anand Swarup <avuppulury@xebia.com>
2012-11-27 16:54:33 +0000

    commit the new classes (missed in the last commit)

---
+++ core/src/main/java/org/eobjects/datacleaner/util/MonitorHttpClientDefaultImpl.java
@@ -0,0 +1,44 @@
+/**
+ * eobjects.org DataCleaner
+ * Copyright (C) 2010 eobjects.org
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.eobjects.datacleaner.util;
+
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+
+public class MonitorHttpClientDefaultImpl implements MonitorHttpClient {
+
+    private final HttpClient _httpClient;
+
+    public MonitorHttpClientDefaultImpl(HttpClient httpClient) {
+        _httpClient = httpClient;
+    }
+
+    @Override
+    public HttpResponse execute(HttpRequest request) throws Exception {
+        if (request instanceof HttpGet) {
+            return _httpClient.execute((HttpGet) request);
+        }
+        return _httpClient.execute((HttpPost) request);
+    }
+
+}

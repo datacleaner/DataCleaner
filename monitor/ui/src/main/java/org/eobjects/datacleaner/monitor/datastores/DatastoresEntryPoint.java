40b48f4491926d820486757d753c1c7e1ba14dcf
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2013-03-06 14:30:08 +0000

    Ticket #1016: Added "New datastore" button to datastore page instead of scheduling page.

---
+++ monitor/ui/src/main/java/org/eobjects/datacleaner/monitor/datastores/DatastoresEntryPoint.java
@@ -0,0 +1,50 @@
+/**
+ * eobjects.org DataCleaner
+ * Copyright (C) 2010 eobjects.org
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.eobjects.datacleaner.monitor.datastores;
+
+import org.eobjects.datacleaner.monitor.shared.ClientConfig;
+import org.eobjects.datacleaner.monitor.shared.DictionaryClientConfig;
+import org.eobjects.datacleaner.monitor.shared.model.TenantIdentifier;
+import org.eobjects.datacleaner.monitor.shared.widgets.CreateDatastoreButton;
+import org.eobjects.datacleaner.monitor.util.ErrorHandler;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * GWT Entry point for the datastores module
+ */
+public class DatastoresEntryPoint implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+        GWT.setUncaughtExceptionHandler(ErrorHandler.getUncaughtExceptionHandler());
+
+        final ClientConfig clientConfig = new DictionaryClientConfig();
+        final TenantIdentifier tenant = clientConfig.getTenant();
+        
+        final CreateDatastoreButton button = new CreateDatastoreButton(tenant);
+
+        final RootPanel rootPanel = RootPanel.get("RootPanelTarget");
+        rootPanel.add(button);
+    }
+
+}

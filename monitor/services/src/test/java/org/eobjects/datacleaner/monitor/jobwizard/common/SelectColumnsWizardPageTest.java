a54e742639fd91214aed14ac0e52bb2fbfbae6e5
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2013-03-09 18:28:30 +0000

    Fixed unittest

--- monitor/services/src/test/java/org/eobjects/datacleaner/monitor/jobwizard/common/SelectColumnsWizardPageTest.java
+++ monitor/services/src/test/java/org/eobjects/datacleaner/monitor/jobwizard/common/SelectColumnsWizardPageTest.java
@@ -33,38 +33,56 @@ import org.eobjects.metamodel.schema.Table;
 
 public class SelectColumnsWizardPageTest extends TestCase {
 
-    public void testGetFormInnerHtml() throws Exception {
-        final Integer pageIndex = 1;
+	public void testGetFormInnerHtml() throws Exception {
+		final Integer pageIndex = 1;
 
-        final Table table = new MutableTable().addColumn(new MutableColumn("foo", ColumnType.INTEGER))
-                .addColumn(new MutableColumn("bar", ColumnType.VARCHAR)).addColumn(new MutableColumn("baz"));
+		final Table table = new MutableTable()
+				.addColumn(new MutableColumn("foo", ColumnType.INTEGER))
+				.addColumn(new MutableColumn("bar", ColumnType.VARCHAR))
+				.addColumn(new MutableColumn("baz"));
 
-        final SelectColumnsWizardPage page = new SelectColumnsWizardPage(pageIndex, table) {
-            @Override
-            protected WizardPageController nextPageController(List<Column> selectedColumns) {
-                throw new IllegalStateException("Should not happen in this test");
-            }
-        };
-        final String formInnerHtml = page.getFormInnerHtml();
-        assertEquals("<div>\n" + "    <p>Please select the source columns of the job:</p>\n" + "\n" + "    <table>\n"
-                + "        <tr>\n" + "            <th>&nbsp;</th>\n" + "            <th>Name</th>\n"
-                + "            <th>Type</th>\n" + "        </tr>\n" + "                <tr>\n"
-                + "            <td><input type=\"checkbox\" name=\"columns\"\n"
-                + "                id=\"column_checkbox_0\" value=\"foo\"\n"
-                + "                title=\"foo\" /></td>\n"
-                + "            <td><label for=\"column_checkbox_0\">foo</label></td>\n"
-                + "            <td>INTEGER</td>\n" + "        </tr>\n" + "        <tr>\n"
-                + "            <td><input type=\"checkbox\" name=\"columns\"\n"
-                + "                id=\"column_checkbox_1\" value=\"bar\"\n"
-                + "                title=\"bar\" /></td>\n"
-                + "            <td><label for=\"column_checkbox_1\">bar</label></td>\n"
-                + "            <td>VARCHAR</td>\n" + "        </tr>\n" + "        <tr>\n"
-                + "            <td><input type=\"checkbox\" name=\"columns\"\n"
-                + "                id=\"column_checkbox_2\" value=\"baz\"\n"
-                + "                title=\"baz\" /></td>\n"
-                + "            <td><label for=\"column_checkbox_2\">baz</label></td>\n" + "            <td>n/a</td>\n"
-                + "        </tr>\n" + "    </table>\n" + "</div>",
-                formInnerHtml.replaceAll("\t", "    ").replaceAll("\r\n", "\n"));
+		final SelectColumnsWizardPage page = new SelectColumnsWizardPage(
+				pageIndex, table) {
+			@Override
+			protected WizardPageController nextPageController(
+					List<Column> selectedColumns) {
+				throw new IllegalStateException(
+						"Should not happen in this test");
+			}
+		};
+		final String formInnerHtml = page.getFormInnerHtml();
+		assertEquals(
+				"<div>\n"
+						+ "    <p>Please select the source columns of the job:</p>\n"
+						+ "\n"
+						+ "    <table>\n"
+						+ "        <tr>\n"
+						+ "            <th>&nbsp;</th>\n"
+						+ "            <th>Name</th>\n"
+						+ "            <th>Type</th>\n"
+						+ "        </tr>\n"
+						+ "        <tr>\n"
+						+ "            <td><input type=\"checkbox\" name=\"columns\"\n"
+						+ "                id=\"column_checkbox_0\" value=\"foo\"\n"
+						+ "                title=\"foo\" /></td>\n"
+						+ "            <td><label for=\"column_checkbox_0\">foo</label></td>\n"
+						+ "            <td>INTEGER</td>\n"
+						+ "        </tr>\n"
+						+ "        <tr>\n"
+						+ "            <td><input type=\"checkbox\" name=\"columns\"\n"
+						+ "                id=\"column_checkbox_1\" value=\"bar\"\n"
+						+ "                title=\"bar\" /></td>\n"
+						+ "            <td><label for=\"column_checkbox_1\">bar</label></td>\n"
+						+ "            <td>VARCHAR</td>\n"
+						+ "        </tr>\n"
+						+ "        <tr>\n"
+						+ "            <td><input type=\"checkbox\" name=\"columns\"\n"
+						+ "                id=\"column_checkbox_2\" value=\"baz\"\n"
+						+ "                title=\"baz\" /></td>\n"
+						+ "            <td><label for=\"column_checkbox_2\">baz</label></td>\n"
+						+ "            <td>n/a</td>\n" + "        </tr>\n"
+						+ "    </table>\n" + "    \n" + "    \n" + "</div>",
+				formInnerHtml.replaceAll("\t", "    ").replaceAll("\r\n", "\n"));
 
-    }
+	}
 }

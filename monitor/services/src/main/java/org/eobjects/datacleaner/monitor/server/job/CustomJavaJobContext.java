7be5e451c883b0144685cb0d5914c7d8a8512cb8
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2013-03-20 14:15:15 +0000

    Ticket #1079: Added early draft "Custom Java job" engine

---
+++ monitor/services/src/main/java/org/eobjects/datacleaner/monitor/server/job/CustomJavaJobContext.java
@@ -0,0 +1,61 @@
+/**
+ * DataCleaner (community edition)
+ * Copyright (C) 2013 Human Inference
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.eobjects.datacleaner.monitor.server.job;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.eobjects.datacleaner.monitor.job.JobContext;
+import org.eobjects.datacleaner.repository.RepositoryFile;
+
+/**
+ * {@link JobContext} for custom Java jobs.
+ */
+public class CustomJavaJobContext implements JobContext {
+
+    private final RepositoryFile _file;
+
+    public CustomJavaJobContext(RepositoryFile file) {
+        _file = file;
+    }
+
+    @Override
+    public String getName() {
+        final int extensionLength = CustomJavaJobEngine.EXTENSION.length();
+        final String filename = _file.getName();
+        return filename.substring(0, filename.length() - extensionLength);
+    }
+
+    @Override
+    public RepositoryFile getJobFile() {
+        return _file;
+    }
+
+    @Override
+    public String getGroupName() {
+        return "Custom Java jobs";
+    }
+
+    @Override
+    public Map<String, String> getVariables() {
+        return Collections.emptyMap();
+    }
+
+}
---
+++ monitor/services/src/main/java/org/eobjects/datacleaner/monitor/server/job/CustomJavaJobEngine.java
@@ -0,0 +1,60 @@
+/**
+ * DataCleaner (community edition)
+ * Copyright (C) 2013 Human Inference
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.eobjects.datacleaner.monitor.server.job;
+
+import java.util.Map;
+
+import org.eobjects.datacleaner.monitor.configuration.TenantContext;
+import org.eobjects.datacleaner.monitor.job.ExecutionLogger;
+import org.eobjects.datacleaner.monitor.job.JobEngine;
+import org.eobjects.datacleaner.monitor.scheduling.model.ExecutionLog;
+import org.eobjects.datacleaner.repository.RepositoryFile;
+import org.springframework.stereotype.Component;
+
+/**
+ * A {@link JobEngine} implementation for running custom Java-based jobs
+ */
+@Component
+public class CustomJavaJobEngine extends AbstractJobEngine<CustomJavaJobContext> {
+
+    public static final String EXTENSION = ".java.job.xml";
+
+    public CustomJavaJobEngine() {
+        super(EXTENSION);
+    }
+
+    @Override
+    public String getJobTypeDisplayName() {
+        return "Custom Java job";
+    }
+
+    @Override
+    protected CustomJavaJobContext getJobContext(TenantContext tenantContext, RepositoryFile file) {
+        return new CustomJavaJobContext(file);
+    }
+
+    @Override
+    public void executeJob(TenantContext context, ExecutionLog execution, ExecutionLogger executionLogger,
+            Map<String, String> variables) throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+}

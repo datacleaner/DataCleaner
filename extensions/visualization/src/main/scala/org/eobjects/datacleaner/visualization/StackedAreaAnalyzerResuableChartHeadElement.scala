8d7b23b941c8c7a7be685e76378be75b595c9a21
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2013-04-09 19:25:41 +0000

    Ticket #1095: Used 'selection' flot plugin to allow zooming in scatter, density and stacked area charts.

--- extensions/visualization/src/main/scala/org/eobjects/datacleaner/visualization/StackedAreaAnalyzerResuableChartHeadElement.scala
+++ extensions/visualization/src/main/scala/org/eobjects/datacleaner/visualization/StackedAreaAnalyzerResuableChartHeadElement.scala
@@ -8,6 +8,7 @@ object StackedAreaAnalyzerResuableChartHeadElement extends HeadElement {
 
   override def toHtml(context: HtmlRenderingContext): String = {
     val flotBaseLocation = FlotChartLocator.getFlotBaseUrl
+    val flotSelectionUrl = FlotChartLocator.getFlotSelectionUrl
     
     return """<script type="text/javascript">
 //<![CDATA[
@@ -15,30 +16,74 @@ function draw_stacked_area_analyzer_chart(chartElement, chartData, retries) {
    
     wait_for_script_load('jQuery', function() {
         importJS('""" + flotBaseLocation + """', 'jQuery.plot', function() {
-            var elem = document.getElementById(chartElement);
-            
-            try {
-                jQuery.plot(elem, chartData, {
-                    series: {
-                        lines: {
-                            active: true,
-                            show: true,
-                            lineWidth: 0,
-                            fill: 1
+            importJS('""" + flotSelectionUrl + """', 'jQuery.plot.plugins[0]', function() {
+                var elem = jQuery("#" + chartElement);
+                var options = {
+                        series: {
+                            lines: {
+                                active: true,
+                                show: true,
+                                lineWidth: 0,
+                                fill: 1
+                            }
+                        },
+                        points: {show:false},
+                        legend: {show: true,position:"se"},
+                        selection: {
+                            mode: "xy"
                         }
-                    },
-                    points: {show:false},
-                    legend: {show: true}
-                });
-            } catch (err) {
-                // error can sometimes occur due to load time issues
-                if (retries > 0) {
-                    retries = retries-1;
-                    draw_stacked_area_analyzer_chart(chartElement, chartData, retries);
+                    };
+                
+                try {
+                    var plot = jQuery.plot(elem, chartData, options);
+                    draw_stacked_area_analyzer_chart_buttons(elem, plot, chartData, options, false);
+                } catch (err) {
+                    // error can sometimes occur due to load time issues
+                    if (retries > 0) {
+                        retries = retries-1;
+                        draw_stacked_area_analyzer_chart_buttons(chartElement, chartData, retries);
+                    }
                 }
-            }
+            });
         });
     });
+}
+
+function draw_stacked_area_analyzer_chart_buttons(elem, plot, chartData, options, zoomed) {
+    var zoomOutButton = jQuery("<div class='stackedAreaChartButton' style='top:10px;'>Zoom out</div>")
+    if (!zoomed) {
+        zoomOutButton.hide();
+    }
+
+    elem.bind("plotselected", function (event, ranges) {
+        plot = $.plot(elem, chartData, jQuery.extend(true, {}, options, {
+            xaxis: {
+                min: ranges.xaxis.from,
+                max: ranges.xaxis.to
+            },
+            yaxis: {
+                min: ranges.yaxis.from,
+                max: ranges.yaxis.to
+            }
+        }));
+        draw_stacked_area_analyzer_chart_buttons(elem, plot, chartData, options, true);
+    });
+            
+    zoomOutButton.appendTo(elem)
+        .click(function (event) {
+            event.preventDefault();
+            plot = $.plot(elem, chartData, jQuery.extend(true, {}, options, {
+                xaxis: {
+                    min: null,
+                    max: null
+                },
+                yaxis: {
+                    min: null,
+                    max: null
+                }
+            }));
+            draw_stacked_area_analyzer_chart_buttons(elem, plot, chartData, options, false);
+        });
 }
 //]]>
 </script>
@@ -46,6 +91,16 @@ function draw_stacked_area_analyzer_chart(chartElement, chartData, retries) {
 .stackedAreaAnalyzerChart {
     height: 550px;
     width: 94%;
+}        
+.stackedAreaChartButton {
+    cursor: pointer;
+    background-color:#333;
+    color:white;
+    padding:3px;
+    font-size:11px;
+    position:absolute;
+    right:10px;
+    border-radius: 3px;
 }
 </style>"""
   }

eabd11c9b87a4ac618b06aae722d8eb8fa5b4638
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2015-01-11 20:10:30 +0100

    Fixed unittest

--- engine/xml-config/src/test/java/org/datacleaner/test/full/scenarios/ComponentsWithoutInputColumnsTest.java
+++ engine/xml-config/src/test/java/org/datacleaner/test/full/scenarios/ComponentsWithoutInputColumnsTest.java
@@ -40,7 +40,7 @@ public class ComponentsWithoutInputColumnsTest extends TestCase {
 
 	public void testScenario() throws Throwable {
 		CsvDatastore datastore = new CsvDatastore("my database",
-				"../../core/src/test/resources/example-name-lengths.csv");
+				"../core/src/test/resources/example-name-lengths.csv");
 		ClasspathScanDescriptorProvider descriptorProvider = new ClasspathScanDescriptorProvider().scanPackage("org.datacleaner", true);
 		AnalyzerBeansConfiguration configuration = new AnalyzerBeansConfigurationImpl()
 				.replace(new DatastoreCatalogImpl(datastore)).replace(descriptorProvider);

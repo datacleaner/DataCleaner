7ce53cd7d83f682c86d56a27ae3e32f35420cbdb
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2015-02-06 14:17:28 +0100

    Fixed license headers.

--- desktop/ui/src/main/java/org/datacleaner/util/DatastoreCreationUtil.java
+++ desktop/ui/src/main/java/org/datacleaner/util/DatastoreCreationUtil.java
@@ -1,3 +1,22 @@
+/**
+ * DataCleaner (community edition)
+ * Copyright (C) 2014 Neopost - Customer Information Management
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
 package org.datacleaner.util;
 
 import java.io.File;
@@ -19,6 +38,7 @@ import org.datacleaner.connection.XmlDatastore;
 import org.datacleaner.user.MutableDatastoreCatalog;
 
 public class DatastoreCreationUtil {
+
     public enum FileDatastoreEnum {
         CSV("csv", "tsv", "txt", "dat"), EXCEL("xls", "xlsx"), ACCESS("mdb"), SAS(), DBASE("dbf"), XML("xml"), JSON(
                 "json"), OPENOFFICE("odb");

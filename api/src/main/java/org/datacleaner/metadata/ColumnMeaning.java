9b85cc1b545e2f4dea3477358db3fc7a83b749f9
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2015-02-20 14:38:03 +0100

    Added an enum with common "ColumnMeaning" values.

---
+++ api/src/main/java/org/datacleaner/metadata/ColumnMeaning.java
@@ -0,0 +1,160 @@
+/**
+ * DataCleaner (community edition)
+ * Copyright (C) 2014 Neopost - Customer Information Management
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.datacleaner.metadata;
+
+import org.apache.metamodel.util.HasName;
+import org.datacleaner.util.HasAliases;
+
+/**
+ * A common and general enum of 'meanings of columns/fields' in a table. This
+ * type is available as a convenience for mapping and registering metadata.
+ */
+public enum ColumnMeaning implements HasName, HasAliases {
+
+    // generic items
+
+    KEY_PRIMARY("Primary Key", "Record ID", "ID", "Identifier", "key", "PKID", "Record key"),
+
+    KEY_FOREIGN("Foreign Key", "FK"),
+
+    // PERSON oriented items
+
+    PERSON_NAME_GIVEN("First name", "Given name", "Forename", "Christian name", "1st name", "Name one"),
+
+    PERSON_NAME_FAMILY("Last name", "Family name", "Surname", "2nd name", "Name two"),
+
+    PERSON_NAME_FULL("Full name", "Name", "Person name"),
+
+    PERSON_GENDER("Gender", "Sex"),
+
+    PERSON_AGE("Age"),
+
+    PERSON_BIRTHDATE("Birthdate"),
+
+    // COMPANY oriented items
+
+    COMPANY_REGISTRATION_NUMBER("Company registration number", "Reg number"),
+
+    COMPANY_NAME("Company name", "Company", "Organization", "Business", "Organisation", "Employer", "Firm",
+            "Workplace", "Works at", "Vendor", "Supplier"),
+
+    // PRODUCT oriented items
+
+    PRODUCT_CODE("Product code", "Product ID"),
+
+    PRODUCT_NAME("Product name", "Product"),
+
+    PRODUCT_LINE("Product line", "Product type", "Product area"),
+
+    PRODUCT_QUANTITY("Product quantity", "Quantity", "Product amount"),
+
+    // MONEY oriented items
+
+    MONEY_AMOUNT("Money amount", "Amount", "Price", "Cost"),
+
+    MONEY_CURRENCY("Money currency", "Currency", "Valuta", "Exchange"),
+
+    // ADDRESS oriented items
+
+    ADDRESS_LINE("Addressline", "Line", "Address"),
+
+    ADDRESS_COUNTRY("Country", "Country code", "Country name", "Land", "Country ISO", "Nation", "Cty", "Nationality"),
+
+    ADDRESS_STATE("State", "State code", "Province", "Postal state"),
+
+    ADDRESS_COUNTY("Region/County", "County name", "County", "County code", "Region"),
+
+    ADDRESS_CITY("City", "Town", "Postal town", "Post town", "Postal city"),
+
+    ADDRESS_POSTAL_CODE("Postal code", "Zip", "Zip code", "Post code", "Post number"),
+
+    ADDRESS_STREET("Street", "Street name", "Thoroughfare", "Road", "Avenue", "Str", "Way"),
+
+    ADDRESS_HOUSE_NUMBER("House number", "House no", "Hausnummer", "huisnummer", "nummer", "Husnummer"),
+
+    ADDRESS_APARTMENT("Apartment/Suite number", "Apartment", "Suite", "Unit", "Floor", "Etage", "Ste", "Lejlighed"),
+
+    // PHONE oriented items
+
+    PHONE_PHONENUMBER("Phone number", "Phone", "Mobile", "Mobile phone", "Cellphone", "*phone*", "Telefon", "Tel.",
+            "Mobil", "*Telefon*", "mobiel", "telefoon", "tel"),
+
+    PHONE_FAX("Fax number", "Fax", "Faxnummer", "fax", "faxnummer"),
+
+    // EMAIL oriented items
+
+    EMAIL_ADDRESS("Email", "Mail", "Email address", "Mail address", "e-mail", "@"),
+
+    // ONLINE oriented items
+
+    ONLINE_WEBSITE("Website", "Website URL", "url", "www", "Homepage", "Homepage URL", "Blog URL", "Blog", "Web",
+            "Homepage"),
+
+    ONLINE_TWITTER("Twitter ID", "Twitter", "Twitter account"),
+
+    ONLINE_FACEBOOK("Facebook ID", "Facebook", "Facebook account"),
+
+    ONLINE_LINKEDIN("LinkedIn ID", "LinkedIn", "LinkedIn account"),
+
+    ;
+
+    private final String _name;
+    private final String[] _aliases;
+
+    // Used for all the fields that are also available for input
+    private ColumnMeaning(final String name, final String... aliases) {
+        _name = name;
+        if (aliases == null) {
+            _aliases = new String[0];
+        } else {
+            _aliases = aliases;
+        }
+    }
+
+    /**
+     * Determines if it is meaningful to map multiple fields to this meaning.
+     * For instance, it makes sense to have multiple address lines, but not
+     * multiple cities (at least in most cases).
+     *
+     * @return
+     */
+    public boolean isMultipleFieldsMappeable() {
+        switch (this) {
+        case ADDRESS_LINE:
+        case EMAIL_ADDRESS:
+        case PHONE_PHONENUMBER:
+        case KEY_FOREIGN:
+        case KEY_PRIMARY:
+            return true;
+        default:
+            return false;
+        }
+    }
+
+    @Override
+    public String getName() {
+        return _name;
+    }
+
+    @Override
+    public String[] getAliases() {
+        return _aliases;
+    }
+}

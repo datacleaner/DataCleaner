7ebced4b9df24932dbd2a9adbb50a0e197d28b8e
Dennis Du Krøger <dennis.dukroger@humaninference.com>
2015-03-13 13:04:40 +0100

    Removed moved stuff

--- extensions/visualization/target/classes/org/eobjects/datacleaner/visualization/VisualizationCategory.scala
+++
@@ -1,9 +0,0 @@
-package org.eobjects.datacleaner.visualization
-
-import org.eobjects.analyzer.beans.api.ComponentCategory
-import org.eobjects.analyzer.beans.categories.AbstractComponentCategory
-
-class VisualizationCategory extends AbstractComponentCategory {
-
-  override def getName() = "Visualization"
-}

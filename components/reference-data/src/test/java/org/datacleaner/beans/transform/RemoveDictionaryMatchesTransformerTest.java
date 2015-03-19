448e229f62e0e81b314eb9c8afa7546988f76b7e
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2015-03-19 22:23:36 +0100

    Issue #341: Initial work done (and works for single word tokens). Still not working for multi-word tokens.

---
+++ components/reference-data/src/test/java/org/datacleaner/beans/transform/RemoveDictionaryMatchesTransformerTest.java
@@ -0,0 +1,59 @@
+/**
+ * DataCleaner (community edition)
+ * Copyright (C) 2014 Neopost - Customer Information Management
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.datacleaner.beans.transform;
+
+import junit.framework.TestCase;
+
+import org.datacleaner.api.InputColumn;
+import org.datacleaner.data.MockInputColumn;
+import org.datacleaner.reference.Dictionary;
+import org.datacleaner.reference.SimpleDictionary;
+import org.junit.Test;
+
+public class RemoveDictionaryMatchesTransformerTest extends TestCase {
+
+    private final InputColumn<String> col = new MockInputColumn<>("Job title");
+    private final Dictionary dictionary = new SimpleDictionary("Title adjectives", "Junior", "Senior", "Lead",
+            "Principal", "Assistant to");
+    private RemoveDictionaryMatchesTransformer transformer;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        transformer = new RemoveDictionaryMatchesTransformer(col, dictionary);
+    }
+
+    @Test
+    public void testGetOutputColumns() throws Exception {
+        assertEquals("OutputColumns[Job title (Title adjectives removed)]", transformer.getOutputColumns().toString());
+    }
+
+    public void testJobTitleScenario() throws Throwable {
+        assertEquals("Software Engineer", transformer.transform("Senior Software Engineer"));
+        assertEquals("Designer", transformer.transform("  Lead   Designer  "));
+        assertEquals("Software Engineer", transformer.transform("Software  Engineer"));
+        assertEquals("Guru of employees", transformer.transform("Principal Senior Lead Guru of Junior employees"));
+    }
+    
+    @Test
+    public void testRemoveMultiTokenPart() throws Exception {
+        assertEquals("CEO", transformer.transform("Assistant to the CEO"));
+    }
+}

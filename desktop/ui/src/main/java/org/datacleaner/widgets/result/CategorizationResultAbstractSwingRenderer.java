0f83c05c733f498f8515abab1d6e85b98a8a6f8f
Dennis Du Krøger <dennis.dukroger@humaninference.com>
2015-04-22 17:23:27 +0200

    Refactor CategorizationResultSwingRenderer to allow easy reuse.

---
+++ desktop/ui/src/main/java/org/datacleaner/widgets/result/CategorizationResultAbstractSwingRenderer.java
@@ -0,0 +1,86 @@
+package org.datacleaner.widgets.result;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.table.DefaultTableModel;
+
+import org.datacleaner.api.AnalyzerResult;
+import org.datacleaner.api.Provided;
+import org.datacleaner.bootstrap.WindowContext;
+import org.datacleaner.panels.DCPanel;
+import org.datacleaner.result.AnnotatedRowsResult;
+import org.datacleaner.result.CategorizationResult;
+import org.datacleaner.result.renderer.AbstractRenderer;
+import org.datacleaner.result.renderer.RendererFactory;
+import org.datacleaner.util.IconUtils;
+import org.datacleaner.util.LabelUtils;
+import org.datacleaner.util.WidgetFactory;
+import org.datacleaner.windows.DetailsResultWindow;
+import org.jfree.data.general.DefaultPieDataset;
+
+public abstract class CategorizationResultAbstractSwingRenderer<R extends CategorizationResult> extends AbstractRenderer<R, JComponent> {
+
+    @Inject
+    @Provided
+    WindowContext windowContext;
+    @Inject
+    @Provided
+    RendererFactory rendererFactory;
+
+    protected int addValue(final int row, final DefaultPieDataset dataset, final DefaultTableModel model, final String desc, final int count, final AnnotatedRowsResult sampleResult) {
+        dataset.setValue(desc, count);
+        model.setValueAt(desc, row, 0);
+        if (sampleResult == null || count == 0) {
+            model.setValueAt(count, row, 1);
+        } else {
+            final DCPanel panel = new DCPanel();
+            panel.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+    
+            final JLabel label = new JLabel(count + "");
+            final JButton button = WidgetFactory.createSmallButton(IconUtils.ACTION_DRILL_TO_DETAIL);
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    drillToGroup(desc, sampleResult);
+                }
+    
+            });
+    
+            panel.add(label);
+            panel.add(Box.createHorizontalStrut(4));
+            panel.add(button);
+    
+            model.setValueAt(panel, row, 1);
+        }
+        return row + 1;
+    }
+
+    private void drillToGroup(String title, AnnotatedRowsResult sampleResult) {
+        List<AnalyzerResult> results = Arrays.<AnalyzerResult> asList(sampleResult);
+        final DetailsResultWindow window = new DetailsResultWindow(title, results, windowContext, rendererFactory);
+        window.open();
+    }
+
+    protected DefaultTableModel prepareModel(CategorizationResult analyzerResult, final DefaultPieDataset dataset) {
+        final DefaultTableModel model = new DefaultTableModel(new Object[] { "Match outcome", LabelUtils.COUNT_LABEL },
+                analyzerResult.getCategoryNames().size());
+        int row = 0;
+        final Collection<String> categoryNames = analyzerResult.getCategoryNames();
+        for (String categoryName : categoryNames) {
+            final AnnotatedRowsResult sample = analyzerResult.getCategoryRowSample(categoryName);
+            final Number count = analyzerResult.getCategoryCount(categoryName);
+            row = addValue(row, dataset, model, categoryName, count.intValue(), sample);
+        }
+        return model;
+    }
+}
--- desktop/ui/src/main/java/org/datacleaner/widgets/result/CategorizationResultSwingRenderer.java
+++ desktop/ui/src/main/java/org/datacleaner/widgets/result/CategorizationResultSwingRenderer.java
@@ -19,38 +19,17 @@
  */
 package org.datacleaner.widgets.result;
 
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-
-import javax.inject.Inject;
-import javax.swing.Box;
-import javax.swing.JButton;
 import javax.swing.JComponent;
-import javax.swing.JLabel;
 import javax.swing.JSplitPane;
 import javax.swing.table.DefaultTableModel;
 
-import org.datacleaner.api.AnalyzerResult;
-import org.datacleaner.api.Provided;
 import org.datacleaner.api.RendererBean;
-import org.datacleaner.bootstrap.WindowContext;
 import org.datacleaner.panels.DCPanel;
-import org.datacleaner.result.AnnotatedRowsResult;
 import org.datacleaner.result.CategorizationResult;
-import org.datacleaner.result.renderer.AbstractRenderer;
-import org.datacleaner.result.renderer.RendererFactory;
 import org.datacleaner.result.renderer.SwingRenderingFormat;
 import org.datacleaner.util.ChartUtils;
-import org.datacleaner.util.IconUtils;
-import org.datacleaner.util.LabelUtils;
-import org.datacleaner.util.WidgetFactory;
 import org.datacleaner.util.WidgetUtils;
 import org.datacleaner.widgets.table.DCTable;
-import org.datacleaner.windows.DetailsResultWindow;
 import org.jdesktop.swingx.VerticalLayout;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -58,31 +37,15 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.data.general.DefaultPieDataset;
 
 @RendererBean(SwingRenderingFormat.class)
-public class CategorizationResultSwingRenderer extends AbstractRenderer<CategorizationResult, JComponent> {
-
-    @Inject
-    @Provided
-    WindowContext windowContext;
-
-    @Inject
-    @Provided
-    RendererFactory rendererFactory;
+public class CategorizationResultSwingRenderer extends CategorizationResultAbstractSwingRenderer<CategorizationResult> {
 
     public CategorizationResultSwingRenderer() {
     }
 
     @Override
     public JComponent render(CategorizationResult analyzerResult) {
-        final DefaultTableModel model = new DefaultTableModel(new Object[] { "Match outcome", LabelUtils.COUNT_LABEL },
-                3);
         final DefaultPieDataset dataset = new DefaultPieDataset();
-        int row = 0;
-        final Collection<String> categoryNames = analyzerResult.getCategoryNames();
-        for (String categoryName : categoryNames) {
-            final AnnotatedRowsResult sample = analyzerResult.getCategoryRowSample(categoryName);
-            final Number count = analyzerResult.getCategoryCount(categoryName);
-            row = addValue(row, dataset, model, categoryName, count.intValue(), sample);
-        }
+        final DefaultTableModel model = prepareModel(analyzerResult, dataset);
 
         final DCTable table = new DCTable(model);
         table.setColumnControlVisible(false);
@@ -106,40 +69,5 @@ public class CategorizationResultSwingRenderer extends AbstractRenderer<Categori
         split.setDividerLocation(550);
 
         return split;
-    }
-
-    private int addValue(final int row, final DefaultPieDataset dataset, final DefaultTableModel model,
-            final String desc, final int count, final AnnotatedRowsResult sampleResult) {
-        dataset.setValue(desc, count);
-        model.setValueAt(desc, row, 0);
-        if (sampleResult == null || count == 0) {
-            model.setValueAt(count, row, 1);
-        } else {
-            final DCPanel panel = new DCPanel();
-            panel.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
-
-            final JLabel label = new JLabel(count + "");
-            final JButton button = WidgetFactory.createSmallButton(IconUtils.ACTION_DRILL_TO_DETAIL);
-            button.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    drillToGroup(desc, sampleResult);
-                }
-
-            });
-
-            panel.add(label);
-            panel.add(Box.createHorizontalStrut(4));
-            panel.add(button);
-
-            model.setValueAt(panel, row, 1);
-        }
-        return row + 1;
-    }
-
-    private void drillToGroup(String title, AnnotatedRowsResult sampleResult) {
-        List<AnalyzerResult> results = Arrays.<AnalyzerResult> asList(sampleResult);
-        final DetailsResultWindow window = new DetailsResultWindow(title, results, windowContext, rendererFactory);
-        window.open();
     }
 }

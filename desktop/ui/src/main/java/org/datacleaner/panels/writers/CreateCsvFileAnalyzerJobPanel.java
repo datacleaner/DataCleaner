0abd811ee83c858f221c017ec0f51faf64e77745
tomaszguzialek <tomasz.guzialek@humaninference.com>
2015-05-11 13:35:22 +0200

    Auto-populating column names in the UI for CSV.

---
+++ desktop/ui/src/main/java/org/datacleaner/panels/writers/CreateCsvFileAnalyzerJobPanel.java
@@ -0,0 +1,67 @@
+/**
+ * DataCleaner (community edition)
+ * Copyright (C) 2013 Human Inference
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.datacleaner.panels.writers;
+
+import org.datacleaner.api.InputColumn;
+import org.datacleaner.descriptors.ConfiguredPropertyDescriptor;
+import org.datacleaner.extension.output.CreateCsvFileAnalyzer;
+import org.datacleaner.job.builder.AnalyzerComponentBuilder;
+import org.datacleaner.job.builder.ComponentBuilder;
+import org.datacleaner.panels.AnalyzerComponentBuilderPanel;
+import org.datacleaner.widgets.properties.MultipleMappedStringsPropertyWidget;
+import org.datacleaner.widgets.properties.PropertyWidget;
+import org.datacleaner.widgets.properties.PropertyWidgetFactory;
+
+public class CreateCsvFileAnalyzerJobPanel extends AnalyzerComponentBuilderPanel {
+
+    private static final long serialVersionUID = 1L;
+    
+    private final MultipleMappedStringsPropertyWidget _mappedWidget;
+    private final ConfiguredPropertyDescriptor _inputColumnsProperty;
+    private final ConfiguredPropertyDescriptor _mappedStringsProperty;
+
+    public CreateCsvFileAnalyzerJobPanel(AnalyzerComponentBuilder<?> analyzerJobBuilder,
+            PropertyWidgetFactory propertyWidgetFactory) {
+        super(analyzerJobBuilder, propertyWidgetFactory);
+
+        _inputColumnsProperty = analyzerJobBuilder.getDescriptor().getConfiguredProperty(CreateCsvFileAnalyzer.PROPERTY_COLUMNS);
+        _mappedStringsProperty = analyzerJobBuilder.getDescriptor().getConfiguredProperty(CreateCsvFileAnalyzer.PROPERTY_FIELD_NAMES);
+
+        _mappedWidget = new MultipleMappedStringsPropertyWidget(analyzerJobBuilder, _inputColumnsProperty,
+                _mappedStringsProperty) {
+            @Override
+            protected String getDefaultMappedString(InputColumn<?> inputColumn) {
+                return inputColumn.getName();
+            }
+        };
+    }
+
+    @Override
+    protected PropertyWidget<?> createPropertyWidget(ComponentBuilder componentBuilder,
+            ConfiguredPropertyDescriptor propertyDescriptor) {
+        if (propertyDescriptor == _inputColumnsProperty) {
+            return _mappedWidget;
+        } else if (propertyDescriptor == _mappedStringsProperty) {
+            return _mappedWidget.getMappedStringsPropertyWidget();
+        }
+        return super.createPropertyWidget(componentBuilder, propertyDescriptor);
+    }
+    
+}

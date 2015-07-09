c43a0b72d3dc48e337549a0e38a6e5483543c8fa
Dennis Du Krøger <dennis.dukroger@humaninference.com>
2015-07-09 14:49:23 +0200

    Restore old import and JavaDoc format

--- components/standardizers/src/main/java/org/datacleaner/beans/standardize/CountryStandardizationTransformer.java
+++ components/standardizers/src/main/java/org/datacleaner/beans/standardize/CountryStandardizationTransformer.java
@@ -1,16 +1,16 @@
 /**
  * DataCleaner (community edition)
  * Copyright (C) 2014 Neopost - Customer Information Management
- * <p>
+ *
  * This copyrighted material is made available to anyone wishing to use, modify,
  * copy, or redistribute it subject to the terms and conditions of the GNU
  * Lesser General Public License, as published by the Free Software Foundation.
- * <p>
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
  * for more details.
- * <p>
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this distribution; if not, write to:
  * Free Software Foundation, Inc.
@@ -19,17 +19,24 @@
  */
 package org.datacleaner.beans.standardize;
 
-import org.datacleaner.api.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.datacleaner.api.Categorized;
+import org.datacleaner.api.Configured;
+import org.datacleaner.api.Description;
+import org.datacleaner.api.HasAnalyzerResult;
+import org.datacleaner.api.InputColumn;
+import org.datacleaner.api.InputRow;
+import org.datacleaner.api.OutputColumns;
+import org.datacleaner.api.Provided;
+import org.datacleaner.api.Transformer;
 import org.datacleaner.components.categories.MatchingAndStandardizationCategory;
 import org.datacleaner.storage.RowAnnotation;
 import org.datacleaner.storage.RowAnnotationFactory;
 import org.datacleaner.util.LabelUtils;
-
-import javax.inject.Inject;
-import javax.inject.Named;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Named("Country standardizer")
 @Description("Allows you to standardize the country names and codes used throughout your database")
@@ -41,24 +48,15 @@ public class CountryStandardizationTransformer implements Transformer, HasAnalyz
     public static final String PROPERTY_DEFAULT_COUNTRY = "Default country";
 
     public final Map<String, RowAnnotation> countryCountMap = new HashMap<>();
-
-    public static enum OutputFormat {
-        ISO2, ISO3, NAME
-    }
-
     @Configured(PROPERTY_COUNTRY_COLUMN)
     InputColumn<String> countryColumn;
-
     @Configured(PROPERTY_OUTPUT_FORMAT)
     OutputFormat outputFormat = OutputFormat.ISO2;
-
     @Configured(value = PROPERTY_DEFAULT_COUNTRY, required = false)
     Country defaultCountry = null;
-
     @Provided
     @Inject
     RowAnnotationFactory _rowAnnotationFactory;
-
     AtomicInteger _unrecognizedCountries = new AtomicInteger(0);
 
     @Override
@@ -119,6 +117,10 @@ public class CountryStandardizationTransformer implements Transformer, HasAnalyz
     @Override
     public CountryStandardizationResult getResult() {
         return new CountryStandardizationResult(_rowAnnotationFactory, countryCountMap, _unrecognizedCountries.intValue());
+    }
+
+    public static enum OutputFormat {
+        ISO2, ISO3, NAME
     }
 
 }

b4a1769ad9a9667fdd7f5a95dd62c4422641b05e
tomaszguzialek <tomasz.guzialek@humaninference.com>
2015-07-30 14:51:14 +0200

    Integration test for reading a path to input datastore from analysis job file.

--- DataCleaner-spark/src/test/java/org/datacleaner/spark/SparkJobLauncherIntegrationTest.java
+++ DataCleaner-spark/src/test/java/org/datacleaner/spark/SparkJobLauncherIntegrationTest.java
@@ -1,18 +1,38 @@
 package org.datacleaner.spark;
 
 import org.datacleaner.configuration.DataCleanerConfiguration;
+import org.datacleaner.job.NoSuchDatastoreException;
+
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class SparkJobLauncherIntegrationTest {
 
     @Test
-    public void testReadConfXmlFromHdfs() {
+    public void testGetInputDatastorePathPositive() {
         String confXmlPath = "hdfs://bigdatavm" + ":" + "9000" + "/user/vagrant/conf.xml";
+        String analysisJobXmlPath = "hdfs://bigdatavm" + ":" + "9000" + "/user/vagrant/hdfs-job.analysis.xml";
         SparkJobLauncher sparkJobLauncher = new SparkJobLauncher(confXmlPath);
+        
         final DataCleanerConfiguration dataCleanerConfiguration = sparkJobLauncher.getDataCleanerConfiguration();
         assertNotNull(dataCleanerConfiguration);
         assertTrue(dataCleanerConfiguration.getDatastoreCatalog().containsDatastore("Instructions.txt"));
+        
+        sparkJobLauncher.launchJob(analysisJobXmlPath);
+    }
+    
+    @Test(expected = NoSuchDatastoreException.class)
+    public void testGetInputDatastorePathNegative() {
+        String confXmlPath = "hdfs://bigdatavm" + ":" + "9000" + "/user/vagrant/conf.xml";
+        String analysisJobXmlPath = "hdfs://bigdatavm" + ":" + "9000" + "/user/vagrant/hdfs-job-datastore-not-exist.analysis.xml";
+        SparkJobLauncher sparkJobLauncher = new SparkJobLauncher(confXmlPath);
+        
+        final DataCleanerConfiguration dataCleanerConfiguration = sparkJobLauncher.getDataCleanerConfiguration();
+        assertNotNull(dataCleanerConfiguration);
+        assertFalse(dataCleanerConfiguration.getDatastoreCatalog().containsDatastore("Non existent name"));
+        
+        sparkJobLauncher.launchJob(analysisJobXmlPath);
     }
 
 }

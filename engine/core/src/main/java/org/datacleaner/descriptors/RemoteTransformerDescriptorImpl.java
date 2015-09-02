14428ebe51b0668d2e59bbb9baafba1e1fafab92
jhorcicka <j.horcicka@gmc.net>
2015-09-02 08:29:09 +0200

    DATACLEANER-ENGINE-CORE * Remote*: @Author tags were removed and licences were added.

--- engine/core/src/main/java/org/datacleaner/descriptors/RemoteTransformerDescriptorImpl.java
+++ engine/core/src/main/java/org/datacleaner/descriptors/RemoteTransformerDescriptorImpl.java
@@ -1,24 +1,34 @@
+/**
+ * DataCleaner (community edition)
+ * Copyright (C) 2014 Neopost - Customer Information Management
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
 package org.datacleaner.descriptors;
 
-import org.datacleaner.api.ComponentCategory;
+import java.util.Arrays;
 import org.datacleaner.api.ComponentSuperCategory;
-import org.datacleaner.api.Transformer;
 import org.datacleaner.components.categories.TransformSuperCategory;
 import org.datacleaner.components.remote.RemoteTransformer;
 
-import java.lang.annotation.Annotation;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
-
 /**
- * @Author jakub
- * @Since 9/1/15
+ @Since 9/1/15
  */
-public class RemoteTransformerDescriptorImpl extends SimpleComponentDescriptor  implements TransformerDescriptor {
-
-    String displayName;
+public class RemoteTransformerDescriptorImpl extends SimpleComponentDescriptor implements TransformerDescriptor {
+    private String displayName;
 
     public RemoteTransformerDescriptorImpl(String displayName, RemoteConfiguredPropertyDescriptorImpl[] configuredProperties) {
         super(RemoteTransformer.class);

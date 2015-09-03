a514b3f05171aaaef41d4f1f0e7411bcf40b0464
jhorcicka <j.horcicka@gmc.net>
2015-09-03 14:13:55 +0200

    DATACLEANER-API * ComponentRESTClient implementation.

--- api/src/main/java/org/datacleaner/components/rest/RESTClientImpl.java
+++ api/src/main/java/org/datacleaner/components/rest/RESTClientImpl.java
@@ -1,10 +1,30 @@
+/**
+ * DataCleaner (community edition)
+ * Copyright (C) 2014 Neopost - Customer Information Management
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
 package org.datacleaner.components.rest;
+
+import javax.ws.rs.core.MediaType;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
-import javax.ws.rs.core.MediaType;
 
 /**
  * @since 03. 09. 2015
@@ -43,10 +63,19 @@ public class RESTClientImpl implements RESTClient {
     @Override
     public String getResponse(String requestBody) {
         WebResource webResource = client.resource(url);
-        webResource.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON);
-        ClientResponse response = webResource.method(httpMethod.name(), ClientResponse.class, requestBody);
+        WebResource.Builder builder = webResource
+                .accept(MediaType.APPLICATION_JSON)
+                .type(MediaType.APPLICATION_JSON);
+        ClientResponse response = null;
 
-        if (response.getStatus() != HttpCode.OK.getCode()) {
+        if (requestBody != null && ! requestBody.isEmpty()) {
+            response = builder.method(httpMethod.name(), ClientResponse.class, requestBody);
+        }
+        else {
+            response = builder.method(httpMethod.name(), ClientResponse.class);
+        }
+
+        if (response.getStatus() != HttpCode.OK.getCode() && response.getStatus() != HttpCode.CREATED.getCode()) {
             throw new RuntimeException("Request failed. HTTP error code: " + response.getStatus());
         }
 

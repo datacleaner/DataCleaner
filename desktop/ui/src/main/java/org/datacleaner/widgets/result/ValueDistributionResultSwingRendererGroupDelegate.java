31b74e26548fb75dca9b4c30b35a9089b716be3d
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2015-09-07 21:38:12 +0200

    Added "gray" synonym to allow slight inconsistency in TL naming

--- desktop/ui/src/main/java/org/datacleaner/widgets/result/ValueDistributionResultSwingRendererGroupDelegate.java
+++ desktop/ui/src/main/java/org/datacleaner/widgets/result/ValueDistributionResultSwingRendererGroupDelegate.java
@@ -129,8 +129,9 @@ final class ValueDistributionResultSwingRendererGroupDelegate {
         _valueColorMap.put("PURPLE", WidgetUtils.ADDITIONAL_COLOR_PURPLE_BRIGHT);
         _valueColorMap.put("CYAN", WidgetUtils.ADDITIONAL_COLOR_CYAN_BRIGHT);
         _valueColorMap.put("BLUE", WidgetUtils.BG_COLOR_BLUE_BRIGHT);
-        _valueColorMap.put("NOT_PROCESSED", WidgetUtils.BG_COLOR_LESS_DARK);
         _valueColorMap.put("GREY", WidgetUtils.BG_COLOR_MEDIUM);
+        _valueColorMap.put("GRAY", WidgetUtils.BG_COLOR_MEDIUM);
+        _valueColorMap.put("NOT_PROCESSED", WidgetUtils.BG_COLOR_LESS_DARK);
         _valueColorMap.put("FAILURE", WidgetUtils.BG_COLOR_DARKEST);
     }
 

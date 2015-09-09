a0b773dbe7ed17629c3d51f36fb5439b3ff0435c
tomaszguzialek <tomasz.guzialek@humaninference.com>
2015-09-09 11:38:39 +0200

    Setting the input column instead of adding an input column when the input does not accept arrays.

--- desktop/ui/src/main/java/org/datacleaner/widgets/visualization/JobGraphLinkPainter.java
+++ desktop/ui/src/main/java/org/datacleaner/widgets/visualization/JobGraphLinkPainter.java
@@ -236,7 +236,8 @@ public class JobGraphLinkPainter {
                         componentBuilder.addInputColumns(getRelevantSourceColumn(sourceColumns, inputProperty),
                                 inputProperty);
                     } else {
-                        componentBuilder.addInputColumn(sourceColumns.get(0), inputProperty);
+//                        componentBuilder.addInputColumn(sourceColumns.get(0), inputProperty);
+                        componentBuilder.setConfiguredProperty(inputProperty, sourceColumns.get(0));
                     }
                     _actions.showConfigurationDialog(componentBuilder);
 

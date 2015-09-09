329c8573eac29028165aa0a078f294c98ed30da4
tomaszguzialek <tomasz.guzialek@humaninference.com>
2015-09-09 16:26:34 +0200

    Reducing top values.

--- components/value-distribution/src/main/java/org/datacleaner/beans/valuedist/ValueDistributionAnalyzerResultReducer.java
+++ components/value-distribution/src/main/java/org/datacleaner/beans/valuedist/ValueDistributionAnalyzerResultReducer.java
@@ -29,8 +29,10 @@ import javax.inject.Inject;
 import org.datacleaner.api.AnalyzerResultReducer;
 import org.datacleaner.api.InputColumn;
 import org.datacleaner.api.Provided;
+import org.datacleaner.result.SingleValueFrequency;
 import org.datacleaner.result.ValueCountList;
 import org.datacleaner.result.ValueCountListImpl;
+import org.datacleaner.result.ValueFrequency;
 import org.datacleaner.storage.InMemoryRowAnnotationFactory;
 import org.datacleaner.storage.RowAnnotation;
 import org.datacleaner.storage.RowAnnotationFactory;
@@ -53,25 +55,49 @@ public class ValueDistributionAnalyzerResultReducer implements AnalyzerResultRed
         final RowAnnotationFactory annotationFactory = new InMemoryRowAnnotationFactory();
         final InputColumn<?>[] highlightedColumns = null;
 
-        SingleValueDistributionResult reducedResult = new SingleValueDistributionResult("", topValues, uniqueValues, 0,
-                0, 0, annotations, new RowAnnotationImpl(), annotationFactory, highlightedColumns);
+        SingleValueDistributionResult reducedResult = new SingleValueDistributionResult(null, topValues, uniqueValues,
+                0, 0, 0, annotations, new RowAnnotationImpl(), annotationFactory, highlightedColumns);
 
         for (ValueDistributionAnalyzerResult partialResult : analyzerResults) {
             if (partialResult instanceof SingleValueDistributionResult) {
-                int reducedTotalCount = reducedResult.getTotalCount() + partialResult.getTotalCount();
-                Collection<String> reducedUniqueValues = reduceUniqueValues(reducedResult.getUniqueValues(),
+                final SingleValueDistributionResult singleResult = (SingleValueDistributionResult) partialResult;
+                final ValueCountList reducedTopValues = reduceTopValues(reducedResult.getTopValues(),
+                        singleResult.getTopValues());
+                final int reducedTotalCount = reducedResult.getTotalCount() + partialResult.getTotalCount();
+                final Collection<String> reducedUniqueValues = reduceUniqueValues(reducedResult.getUniqueValues(),
                         partialResult.getUniqueValues());
 
-                reducedResult = new SingleValueDistributionResult(reducedResult.getName(),
-                        reducedResult.getTopValues(), reducedUniqueValues, reducedUniqueValues.size(),
-                        reducedResult.getDistinctCount(), reducedTotalCount, annotations, new RowAnnotationImpl(),
-                        annotationFactory, highlightedColumns);
+                reducedResult = new SingleValueDistributionResult(reducedResult.getName(), reducedTopValues,
+                        reducedUniqueValues, reducedUniqueValues.size(), reducedResult.getDistinctCount(),
+                        reducedTotalCount, annotations, new RowAnnotationImpl(), annotationFactory, highlightedColumns);
             } else {
                 // TODO: Disregard grouped results for now
             }
         }
 
         return reducedResult;
+    }
+
+    private ValueCountList reduceTopValues(ValueCountList topValues1, ValueCountList topValues2) {
+        if (topValues1.getValueCounts().isEmpty()) {
+            return topValues2;
+        }
+
+        ValueCountListImpl topValuesImpl1 = (ValueCountListImpl) topValues1;
+        ValueCountListImpl topValuesImpl2 = (ValueCountListImpl) topValues2;
+
+        for (ValueFrequency valueFrequency2 : topValuesImpl2.getValueCounts()) {
+            for (ValueFrequency valueFrequency1 : topValuesImpl1.getValueCounts()) {
+                if (valueFrequency1.getName().equals(valueFrequency2.getName())) {
+                    topValuesImpl1.getValueCounts().remove(valueFrequency1);
+                    topValuesImpl1.register(new SingleValueFrequency(valueFrequency2.getValue(), valueFrequency2
+                            .getCount() + valueFrequency1.getCount()));
+                    break;
+                }
+            }
+        }
+
+        return topValuesImpl1;
     }
 
     private Collection<String> reduceUniqueValues(Collection<String> uniqueValues1, Collection<String> uniqueValues2) {

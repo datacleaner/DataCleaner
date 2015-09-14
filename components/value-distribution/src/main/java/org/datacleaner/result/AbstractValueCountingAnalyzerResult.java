7d14f241583f5a9b50dd487e94c15875d5679f5d
tomaszguzialek <tomasz.guzialek@humaninference.com>
2015-09-14 15:48:10 +0200

    Fixing tabulation.

--- components/value-distribution/src/main/java/org/datacleaner/result/AbstractValueCountingAnalyzerResult.java
+++ components/value-distribution/src/main/java/org/datacleaner/result/AbstractValueCountingAnalyzerResult.java
@@ -32,106 +32,112 @@ import org.datacleaner.util.LabelUtils;
  * An abstract implementation of {@link ValueCountingAnalyzerResult} which
  * implements the most important metric: The value count.
  */
-public abstract class AbstractValueCountingAnalyzerResult implements ValueCountingAnalyzerResult {
+public abstract class AbstractValueCountingAnalyzerResult implements
+		ValueCountingAnalyzerResult {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Metric(value = "Value count", supportsInClause = true)
-    public final QueryParameterizableMetric getValueCount() {
-        return new QueryParameterizableMetric() {
+	@Metric(value = "Value count", supportsInClause = true)
+	public final QueryParameterizableMetric getValueCount() {
+		return new QueryParameterizableMetric() {
 
-            @Override
-            public Collection<String> getParameterSuggestions() {
-                final Collection<ValueFrequency> valueCounts = AbstractValueCountingAnalyzerResult.this
-                        .getValueCounts();
-                final List<String> result = CollectionUtils.map(valueCounts, new Func<ValueFrequency, String>() {
-                    @Override
-                    public String eval(ValueFrequency vc) {
-                        return vc.getName();
-                    }
-                });
-                result.remove(null);
-                result.remove(LabelUtils.NULL_LABEL);
-                result.remove(LabelUtils.UNEXPECTED_LABEL);
-                return result;
-            }
+			@Override
+			public Collection<String> getParameterSuggestions() {
+				final Collection<ValueFrequency> valueCounts = AbstractValueCountingAnalyzerResult.this
+						.getValueCounts();
+				final List<String> result = CollectionUtils.map(valueCounts,
+						new Func<ValueFrequency, String>() {
+							@Override
+							public String eval(ValueFrequency vc) {
+								return vc.getName();
+							}
+						});
+				result.remove(null);
+				result.remove(LabelUtils.NULL_LABEL);
+				result.remove(LabelUtils.UNEXPECTED_LABEL);
+				return result;
+			}
 
-            @Override
-            public int getTotalCount() {
-                return AbstractValueCountingAnalyzerResult.this.getTotalCount();
-            }
+			@Override
+			public int getTotalCount() {
+				return AbstractValueCountingAnalyzerResult.this.getTotalCount();
+			}
 
-            @Override
-            public int getInstanceCount(String instance) {
-                Integer count = getCount(instance);
-                if (count == null) {
-                    return 0;
-                }
-                return count;
-            }
-        };
-    }
+			@Override
+			public int getInstanceCount(String instance) {
+				Integer count = getCount(instance);
+				if (count == null) {
+					return 0;
+				}
+				return count;
+			}
+		};
+	}
 
-    @Override
-    public Collection<ValueFrequency> getReducedValueFrequencies(final int preferredMaximum) {
-        final Collection<ValueFrequency> original = getValueCounts();
-        
-        final Collection<ValueFrequency> result = new TreeSet<ValueFrequency>(original);
+	@Override
+	public Collection<ValueFrequency> getReducedValueFrequencies(
+			final int preferredMaximum) {
+		final Collection<ValueFrequency> original = getValueCounts();
 
-        if (original.size() <= preferredMaximum) {
-            // check if any composite value freq's can be exploded
-            for (ValueFrequency valueFrequency : original) {
-                if (valueFrequency.isComposite()) {
-                    List<ValueFrequency> children = valueFrequency.getChildren();
-                    if (children != null) {
-                        if (result.size() - 1 + children.size() <= preferredMaximum) {
-                            // replace with children
-                            result.remove(valueFrequency);
-                            result.addAll(children);
-                        }
-                    }
-                }
-            }
+		final Collection<ValueFrequency> result = new TreeSet<ValueFrequency>(
+				original);
 
-            return result;
-        } else {
-        	return original;
-        }
-    }
+		if (original.size() <= preferredMaximum) {
+			// check if any composite value freq's can be exploded
+			for (ValueFrequency valueFrequency : original) {
+				if (valueFrequency.isComposite()) {
+					List<ValueFrequency> children = valueFrequency
+							.getChildren();
+					if (children != null) {
+						if (result.size() - 1 + children.size() <= preferredMaximum) {
+							// replace with children
+							result.remove(valueFrequency);
+							result.addAll(children);
+						}
+					}
+				}
+			}
+			return result;
+		} else {
+			return original;
+		}
+	}
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Value distribution for: ");
-        sb.append(getName());
-        appendToString(sb, this, 4);
-        return sb.toString();
-    }
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Value distribution for: ");
+		sb.append(getName());
+		appendToString(sb, this, 4);
+		return sb.toString();
+	}
 
-    /**
-     * Appends a string representation with a maximum amount of entries
-     * 
-     * @param sb
-     *            the StringBuilder to append to
-     * 
-     * @param maxEntries
-     * @return
-     */
-    protected void appendToString(StringBuilder sb, ValueCountingAnalyzerResult groupResult, int maxEntries) {
-        if (maxEntries != 0) {
-            Collection<ValueFrequency> valueCounts = groupResult.getValueCounts();
-            for (ValueFrequency valueCount : valueCounts) {
-                sb.append("\n - ");
-                sb.append(valueCount.getName());
-                sb.append(": ");
-                sb.append(valueCount.getCount());
+	/**
+	 * Appends a string representation with a maximum amount of entries
+	 * 
+	 * @param sb
+	 *            the StringBuilder to append to
+	 * 
+	 * @param maxEntries
+	 * @return
+	 */
+	protected void appendToString(StringBuilder sb,
+			ValueCountingAnalyzerResult groupResult, int maxEntries) {
+		if (maxEntries != 0) {
+			Collection<ValueFrequency> valueCounts = groupResult
+					.getValueCounts();
+			for (ValueFrequency valueCount : valueCounts) {
+				sb.append("\n - ");
+				sb.append(valueCount.getName());
+				sb.append(": ");
+				sb.append(valueCount.getCount());
 
-                maxEntries--;
-                if (maxEntries == 0) {
-                    sb.append("\n ...");
-                    break;
-                }
-            }
-        }
-    }
+				maxEntries--;
+				if (maxEntries == 0) {
+					sb.append("\n ...");
+					break;
+				}
+			}
+		}
+	}
 }

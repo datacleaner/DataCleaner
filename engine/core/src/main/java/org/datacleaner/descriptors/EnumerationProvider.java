4006bbdaed83ea2b946697da52a237c687a3b4e6
Jakub Neubauer <jakub.neubauer@gmail.com>
2015-09-23 16:57:09 +0200

    fixed license headers

--- engine/core/src/main/java/org/datacleaner/descriptors/EnumerationProvider.java
+++ engine/core/src/main/java/org/datacleaner/descriptors/EnumerationProvider.java
@@ -1,16 +1,16 @@
 /**
  * DataCleaner (community edition)
  * Copyright (C) 2014 Neopost - Customer Information Management
- * <p>
+ *
  * This copyrighted material is made available to anyone wishing to use, modify,
  * copy, or redistribute it subject to the terms and conditions of the GNU
  * Lesser General Public License, as published by the Free Software Foundation.
- * <p>
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
  * for more details.
- * <p>
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this distribution; if not, write to:
  * Free Software Foundation, Inc.

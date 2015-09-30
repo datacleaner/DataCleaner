cfb37ca346d8dfe18aff2c28bb2f95453f87a274
ClaudiaPHI <claudia.pesu@humaninference.com>
2015-09-30 17:06:15 +0200

    initial work for boolean analyzer reducer

---
+++ components/basic-analyzers/src/main/java/org/datacleaner/beans/BooleanAnalyzerReducer.java
@@ -0,0 +1,14 @@
+package org.datacleaner.beans;
+
+import java.util.Collection;
+
+import org.datacleaner.api.AnalyzerResultReducer;
+
+public class BooleanAnalyzerReducer implements AnalyzerResultReducer<BooleanAnalyzerResult> {
+
+    @Override
+    public BooleanAnalyzerResult reduce(Collection<? extends BooleanAnalyzerResult> partialResults) {
+       
+    }
+
+}

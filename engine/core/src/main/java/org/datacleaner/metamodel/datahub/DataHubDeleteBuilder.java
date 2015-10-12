d9f0f149caba52838cfc2a46bb0c6b2c741cf39b
HettyK <hetty@Hettyk-think.humaninference.com>
2015-10-12 12:58:36 +0200

    Made some local variables final

--- engine/core/src/main/java/org/datacleaner/metamodel/datahub/DataHubDeleteBuilder.java
+++ engine/core/src/main/java/org/datacleaner/metamodel/datahub/DataHubDeleteBuilder.java
@@ -27,7 +27,13 @@ import org.apache.metamodel.query.FilterItem;
 import org.apache.metamodel.schema.Table;
 
 public class DataHubDeleteBuilder extends AbstractRowDeletionBuilder {
+    /**
+     * Column that should be present in the where clause for golden records
+     */
     private static final String GR_ID_COLUMN_NAME = "gr_id";
+    /**
+     * Columns that should be present in the where clause for source records 
+     */
     private static final String SOURCE_ID_COLUMN_NAME = "source_id";
     private static final String SOURCE_NAME_COLUMN_NAME = "source_name";
 
@@ -40,11 +46,11 @@ public class DataHubDeleteBuilder extends AbstractRowDeletionBuilder {
 
     @Override
     public void execute() throws MetaModelException {
-        List<FilterItem> whereItems = getWhereItems();
+        final List<FilterItem> whereItems = getWhereItems();
         if (whereItems == null || whereItems.size() == 0) {
             throw new IllegalArgumentException("Delete requires a condition.");
         }
-        String firstColumnName = getConditionColumnName(whereItems.get(0));
+        final String firstColumnName = getConditionColumnName(whereItems.get(0));
         if (firstColumnName.equals(GR_ID_COLUMN_NAME)) {
             deleteGoldenRecord(whereItems);
         } else if (firstColumnName.equalsIgnoreCase(SOURCE_ID_COLUMN_NAME)) {
@@ -60,10 +66,10 @@ public class DataHubDeleteBuilder extends AbstractRowDeletionBuilder {
                     "Delete must be executed on a SourceRecordsGoldenRecordFormat table using " + SOURCE_ID_COLUMN_NAME
                     + " and " + SOURCE_NAME_COLUMN_NAME + " as condition values.");
        }
-        FilterItem firstWhereItem = whereItems.get(0);
-        FilterItem secondWhereItem = whereItems.get(1);
+        final FilterItem firstWhereItem = whereItems.get(0);
+        final FilterItem secondWhereItem = whereItems.get(1);
 
-        String secondColumnName = getConditionColumnName(secondWhereItem);
+        final String secondColumnName = getConditionColumnName(secondWhereItem);
         if (SOURCE_NAME_COLUMN_NAME.equals(secondColumnName)) {
             String id = getConditionColumnValue(firstWhereItem);
             String sourceName = getConditionColumnValue(secondWhereItem);
@@ -80,8 +86,8 @@ public class DataHubDeleteBuilder extends AbstractRowDeletionBuilder {
             throw new IllegalArgumentException("Delete requires the " + GR_ID_COLUMN_NAME
                     + " as the sole condition value.");
         }
-        FilterItem whereItem = whereItems.get(0);
-        String grId = getConditionColumnValue(whereItem);
+        final FilterItem whereItem = whereItems.get(0);
+        final String grId = getConditionColumnValue(whereItem);
         _callback.executeDeleteGoldenRecord(grId);
     }
 

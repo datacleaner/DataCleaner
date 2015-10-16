43831ac03f125fc738b8afc35babb6592d3ed4c9
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2015-10-16 09:17:29 +0200

    Added license headers and javadocs

--- desktop/ui/src/main/java/org/datacleaner/documentation/SuperCategoryDocumentationWrapper.java
+++ desktop/ui/src/main/java/org/datacleaner/documentation/SuperCategoryDocumentationWrapper.java
@@ -1,3 +1,22 @@
+/**
+ * DataCleaner (community edition)
+ * Copyright (C) 2014 Neopost - Customer Information Management
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
 package org.datacleaner.documentation;
 
 import java.util.ArrayList;
@@ -10,6 +29,11 @@ import org.datacleaner.api.ComponentCategory;
 import org.datacleaner.api.ComponentSuperCategory;
 import org.datacleaner.util.HasNameComparator;
 
+/**
+ * A wrapper around the {@link ComponentSuperCategory} object to make it easier
+ * for the documentation template to get to certain aspects that should be
+ * presented in the documentation.
+ */
 public class SuperCategoryDocumentationWrapper implements Comparable<SuperCategoryDocumentationWrapper> {
 
     private final ComponentSuperCategory _superCategory;

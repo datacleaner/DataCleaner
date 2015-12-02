d4079cba4ea4f012128a85c07a56149dcd1a4fe3
Dennis Du Krøger <dennis.dukroger@humaninference.com>
2015-12-02 13:00:05 +0100

    Add transformer for subtracting text.
    This will add a new transformer, capable of subtracting substrings from a base text.
    
     Fixes #945

---
+++ components/basic-transformers/src/main/java/org/datacleaner/beans/transform/TextSubtractTransformer.java
@@ -0,0 +1,83 @@
+/**
+ * DataCleaner (community edition)
+ * Copyright (C) 2014 Neopost - Customer Information Management
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.datacleaner.beans.transform;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+import javax.inject.Named;
+
+import org.datacleaner.api.Categorized;
+import org.datacleaner.api.Configured;
+import org.datacleaner.api.Description;
+import org.datacleaner.api.InputColumn;
+import org.datacleaner.api.InputRow;
+import org.datacleaner.api.OutputColumns;
+import org.datacleaner.api.Transformer;
+import org.datacleaner.components.categories.TextCategory;
+import org.datacleaner.components.convert.ConvertToStringTransformer;
+
+@Named("TextSubtractor")
+@Description("Subtracts one or more values from a base text")
+@Categorized(TextCategory.class)
+public class TextSubtractTransformer implements Transformer {
+    @Configured("Base text column")
+    @Description("Column containing the text to subtract from")
+    InputColumn<String> baseColumn;
+
+    @Configured("Substring columns")
+    @Description("Columns containing the substrings to remove from the base text")
+    InputColumn<?>[] substringColumns;
+
+    @Configured(value="Whole words only", required = false)
+    @Description("If set, only whole words (surrounded by whitespace or punctuation) will be removed")
+    boolean wholeWordsOnly = false;
+
+    @Override
+    public OutputColumns getOutputColumns() {
+        return new OutputColumns(String.class, baseColumn.getName() + " (subtracted)");
+    }
+
+    @Override
+    public String[] transform(final InputRow inputRow) {
+        String subtractedString = inputRow.getValue(baseColumn);
+        for (final InputColumn<?> inputColumn : substringColumns) {
+            if (inputColumn.getDataType() == List.class) {
+                final List<?> list = (List) inputRow.getValue(inputColumn);
+                for (final Object element : list) {
+                    subtractedString = subtract(subtractedString, element);
+                }
+            } else {
+                subtractedString = subtract(subtractedString, inputRow.getValue(inputColumn));
+            }
+        }
+
+        return new String[] { subtractedString };
+    }
+
+    private String subtract(final String subtractedString, final Object element) {
+        String stringElement = ConvertToStringTransformer.transformValue(element);
+        if (wholeWordsOnly) {
+            return subtractedString.replaceAll("\\b" + Pattern.quote(stringElement) + "\\b", "");
+        } else {
+            return subtractedString.replace(stringElement, "");
+        }
+    }
+}
---
+++ components/basic-transformers/src/test/java/org/datacleaner/beans/transform/TextSubtractTransformerTest.java
@@ -0,0 +1,80 @@
+/**
+ * DataCleaner (community edition)
+ * Copyright (C) 2014 Neopost - Customer Information Management
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.datacleaner.beans.transform;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.datacleaner.api.InputColumn;
+import org.datacleaner.data.MockInputColumn;
+import org.datacleaner.data.MockInputRow;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TextSubtractTransformerTest {
+
+    private TextSubtractTransformer _t;
+    private MockInputRow _inputRow;
+
+    @Before
+    public void before() {
+        final InputColumn<String> baseColumn = new MockInputColumn<>("baseCol", String.class);
+        final InputColumn<?> stringColumn = new MockInputColumn<>("stringCol", String.class);
+        final InputColumn<?> numberColumn = new MockInputColumn<>("numberCol", Number.class);
+        final InputColumn<?> listColumn = new MockInputColumn<>("listCol", List.class);
+        // Just to test that other types doesn't sent it spinning
+        final InputColumn<?> booleanColumn = new MockInputColumn<>("booleanCol", Boolean.class);
+
+        final InputColumn[] subtractColumns =
+                new InputColumn[] { stringColumn, numberColumn, listColumn, booleanColumn };
+
+        _inputRow = new MockInputRow().put(baseColumn, "hello goodbye, 5, a, 2, c, true, false")
+                .put(subtractColumns[0], "bye").put(subtractColumns[1], 5).put(subtractColumns[2],
+                        Arrays.asList("a", 2, false)).put(subtractColumns[3], true);
+
+        _t = new TextSubtractTransformer();
+        _t.baseColumn = baseColumn;
+        _t.substringColumns = subtractColumns;
+    }
+
+    @Test
+    public void testGetOutputColumns() {
+        Assert.assertEquals(1, _t.getOutputColumns().getColumnCount());
+        Assert.assertEquals(String.class, _t.getOutputColumns().getColumnType(0));
+        Assert.assertEquals("baseCol (subtracted)", _t.getOutputColumns().getColumnName(0));
+    }
+
+    @Test
+    public void testTransformSimple() {
+        String[] result = _t.transform(_inputRow);
+        Assert.assertEquals(1, result.length);
+        Assert.assertEquals("hello good, , , , c, , flse", result[0]);
+    }
+
+    @Test
+    public void testTransformWholeWords() {
+        _t.wholeWordsOnly = true;
+        String[] result = _t.transform(_inputRow);
+        Assert.assertEquals(1, result.length);
+        Assert.assertEquals("hello goodbye, , , , c, , ", result[0]);
+    }
+}

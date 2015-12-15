fcc591096499f734468c672045806c975eb44ea5
jhorcicka <j.horcicka@gmc.net>
2015-12-15 10:07:41 +0100

    * DescriptorProviderState: hash and equals via Objects.

--- api/src/main/java/org/datacleaner/descriptors/DescriptorProviderState.java
+++ api/src/main/java/org/datacleaner/descriptors/DescriptorProviderState.java
@@ -19,6 +19,8 @@
  */
 package org.datacleaner.descriptors;
 
+import java.util.Objects;
+
 /**
  * @since 01. 12. 2015
  */
@@ -47,32 +49,11 @@ public class DescriptorProviderState {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-
-        if (!(o instanceof DescriptorProviderState)) {
-            return false;
-        }
-
-        DescriptorProviderState that = (DescriptorProviderState) o;
-
-        if (level != that.level) {
-            return false;
-        }
-
-        if (message != null ? !message.equals(that.message) : that.message != null) {
-            return false;
-        }
-
-        return true;
+        return Objects.equals(this, o);
     }
 
     @Override
     public int hashCode() {
-        int result = level != null ? level.hashCode() : 0;
-        result = 31 * result + (message != null ? message.hashCode() : 0);
-
-        return result;
+        return Objects.hashCode(this);
     }
 }

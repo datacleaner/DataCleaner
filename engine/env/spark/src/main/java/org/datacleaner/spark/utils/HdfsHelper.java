83a6b6055f44a583aac13af5f94d3bcda845de09
kaspers <kaspers@kasper-think.humaninference.com>
2015-12-20 21:19:57 +0100

    Added missing license and javadoc

--- engine/env/spark/src/main/java/org/datacleaner/spark/utils/HdfsHelper.java
+++ engine/env/spark/src/main/java/org/datacleaner/spark/utils/HdfsHelper.java
@@ -1,3 +1,22 @@
+/**
+ * DataCleaner (community edition)
+ * Copyright (C) 2014 Neopost - Customer Information Management
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
 package org.datacleaner.spark.utils;
 
 import java.io.InputStream;
@@ -13,6 +32,9 @@ import org.datacleaner.util.HadoopResource;
 
 import com.google.common.base.Strings;
 
+/**
+ * Helper class for interacting with HDFS.
+ */
 public class HdfsHelper {
 
     private final Configuration _hadoopConfiguration;

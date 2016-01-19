2051ff2d07ce7041e5c68e3c68a96be43415a128
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2016-01-19 23:33:07 +0100

    Added license header

--- monitor/ui/src/main/java/org/datacleaner/monitor/rewrite/RewriteConfigurationProvider.java
+++ monitor/ui/src/main/java/org/datacleaner/monitor/rewrite/RewriteConfigurationProvider.java
@@ -1,3 +1,22 @@
+/**
+ * DataCleaner (community edition)
+ * Copyright (C) 2014 Neopost - Customer Information Management
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
 package org.datacleaner.monitor.rewrite;
 
 import javax.servlet.ServletContext;
@@ -9,7 +28,7 @@ import org.ocpsoft.rewrite.servlet.config.HttpConfigurationProvider;
 import org.ocpsoft.rewrite.servlet.config.rule.Join;
 
 /**
- * Defines URL rewriting rules for the website
+ * Defines URL rewriting rules for the DataCleaner monitor
  */
 @RewriteConfiguration
 public class RewriteConfigurationProvider extends HttpConfigurationProvider {

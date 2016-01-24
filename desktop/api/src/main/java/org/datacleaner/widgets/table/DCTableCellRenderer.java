21ed3d78166479849860f3e34ff9d2715248cedc
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2016-01-24 16:57:02 +0100

    Fixed indentation

--- desktop/api/src/main/java/org/datacleaner/widgets/table/DCTableCellRenderer.java
+++ desktop/api/src/main/java/org/datacleaner/widgets/table/DCTableCellRenderer.java
@@ -47,95 +47,95 @@ import org.slf4j.LoggerFactory;
 
 public class DCTableCellRenderer implements TableCellRenderer {
 
-	private static final Logger logger = LoggerFactory.getLogger(DCTableCellRenderer.class);
+    private static final Logger logger = LoggerFactory.getLogger(DCTableCellRenderer.class);
 
-	private final DCTable _table;
-	private final Map<Integer, Alignment> _alignmentOverrides;
-	private final DefaultTableCellRenderer _delegate;
+    private final DCTable _table;
+    private final Map<Integer, Alignment> _alignmentOverrides;
+    private final DefaultTableCellRenderer _delegate;
 
-	public DCTableCellRenderer(DCTable table) {
-		super();
-		_table = table;
-		_alignmentOverrides = new HashMap<Integer, Alignment>();
-		_delegate = new DefaultTableCellRenderer();
-	}
+    public DCTableCellRenderer(DCTable table) {
+        super();
+        _table = table;
+        _alignmentOverrides = new HashMap<Integer, Alignment>();
+        _delegate = new DefaultTableCellRenderer();
+    }
 
-	@Override
-	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
-			int row, int column) {
-		logger.debug("getTableCellRendererComponent({},{})", row, column);
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+            int row, int column) {
+        logger.debug("getTableCellRendererComponent({},{})", row, column);
 
-		if (value != null && value.getClass().isArray()) {
+        if (value != null && value.getClass().isArray()) {
             // arrays are printed nicely this way
             value = ArrayUtils.toString(value);
-		}
+        }
 
-		// icons are displayed as labels
-		if (value instanceof Icon) {
-			final JLabel label = new JLabel((Icon) value);
-			label.setOpaque(true);
-			value = label;
-		}
-		
-		if (value instanceof Clob) {
-		    final Clob clob = (Clob) value;
-		    try {
-		        final Reader reader = clob.getCharacterStream();
-		        value = FileHelper.readAsString(reader);
-		    } catch (Exception e) {
-		        logger.error("Failed to read String from CLOB: {}", clob, e);
-		    }
-		}
+        // icons are displayed as labels
+        if (value instanceof Icon) {
+            final JLabel label = new JLabel((Icon) value);
+            label.setOpaque(true);
+            value = label;
+        }
 
-		final Component result;
+        if (value instanceof Clob) {
+            final Clob clob = (Clob) value;
+            try {
+                final Reader reader = clob.getCharacterStream();
+                value = FileHelper.readAsString(reader);
+            } catch (Exception e) {
+                logger.error("Failed to read String from CLOB: {}", clob, e);
+            }
+        }
 
-		// render components directly
-		if (value instanceof JComponent) {
-			final JComponent component = (JComponent) value;
+        final Component result;
 
-			component.setOpaque(true);
+        // render components directly
+        if (value instanceof JComponent) {
+            final JComponent component = (JComponent) value;
 
-			if (component.getMouseListeners().length == 0) {
-				component.addMouseListener(new MouseAdapter() {
-					@Override
-					public void mouseClicked(MouseEvent e) {
-						MouseEvent newEvent = SwingUtilities.convertMouseEvent(component, e, _table);
-						_table.consumeMouseClick(newEvent);
-					}
-				});
-			}
-			
-			result = component;
-		} else {
-			result = _delegate.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-			assert result instanceof JLabel;
-		}
+            component.setOpaque(true);
 
-		// alignment is applied to all labels or panels (with flowlayout)
-		Alignment alignment = _alignmentOverrides.get(column);
-		if (alignment == null) {
-			alignment = Alignment.LEFT;
-		}
+            if (component.getMouseListeners().length == 0) {
+                component.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        MouseEvent newEvent = SwingUtilities.convertMouseEvent(component, e, _table);
+                        _table.consumeMouseClick(newEvent);
+                    }
+                });
+            }
 
-		// set alignment
-		if (value instanceof JPanel) {
-			final LayoutManager layout = ((JPanel) value).getLayout();
-			if (layout instanceof FlowLayout) {
-				final FlowLayout flowLayout = (FlowLayout) layout;
-				flowLayout.setAlignment(alignment.getFlowLayoutAlignment());
-			}
-		} else if (result instanceof JLabel) {
-			final JLabel label = (JLabel) result;
-			label.setHorizontalAlignment(alignment.getSwingContstantsAlignment());
+            result = component;
+        } else {
+            result = _delegate.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+            assert result instanceof JLabel;
+        }
 
-			WidgetUtils.setAppropriateFont(label);
-		}
+        // alignment is applied to all labels or panels (with flowlayout)
+        Alignment alignment = _alignmentOverrides.get(column);
+        if (alignment == null) {
+            alignment = Alignment.LEFT;
+        }
 
-		return result;
-	}
+        // set alignment
+        if (value instanceof JPanel) {
+            final LayoutManager layout = ((JPanel) value).getLayout();
+            if (layout instanceof FlowLayout) {
+                final FlowLayout flowLayout = (FlowLayout) layout;
+                flowLayout.setAlignment(alignment.getFlowLayoutAlignment());
+            }
+        } else if (result instanceof JLabel) {
+            final JLabel label = (JLabel) result;
+            label.setHorizontalAlignment(alignment.getSwingContstantsAlignment());
 
-	public void setAlignment(int column, Alignment alignment) {
-		_alignmentOverrides.put(column, alignment);
-	}
+            WidgetUtils.setAppropriateFont(label);
+        }
+
+        return result;
+    }
+
+    public void setAlignment(int column, Alignment alignment) {
+        _alignmentOverrides.put(column, alignment);
+    }
 
 }

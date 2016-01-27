0f054f8a0c492bb2c8e48e2d02d9feec1132332e
claudiap <Claudia.Pesu@HumanInference.com>
2016-01-27 10:59:10 +0100

    made a functional neo4j datastore dialog

--- desktop/ui/src/main/java/org/datacleaner/windows/Neo4jDatastoreDialog.java
+++ desktop/ui/src/main/java/org/datacleaner/windows/Neo4jDatastoreDialog.java
@@ -19,42 +19,53 @@
  */
 package org.datacleaner.windows;
 
+import java.util.List;
+import java.util.Map.Entry;
+
+import javax.inject.Inject;
+import javax.swing.JComponent;
 import javax.swing.JPasswordField;
 import javax.swing.event.DocumentEvent;
 
 import org.apache.metamodel.neo4j.Neo4jDataContext;
 import org.apache.metamodel.schema.Schema;
 import org.datacleaner.bootstrap.WindowContext;
+import org.datacleaner.connection.DatastoreConnection;
 import org.datacleaner.connection.Neo4jDatastore;
+import org.datacleaner.guice.Nullable;
 import org.datacleaner.user.MutableDatastoreCatalog;
 import org.datacleaner.user.UserPreferences;
 import org.datacleaner.util.DCDocumentListener;
 import org.datacleaner.util.IconUtils;
+import org.datacleaner.util.ImmutableEntry;
+import org.datacleaner.util.NumberDocument;
 import org.datacleaner.util.SchemaFactory;
 import org.datacleaner.util.StringUtils;
 import org.datacleaner.util.WidgetFactory;
 import org.jdesktop.swingx.JXTextField;
 
-public class Neo4jDatastoreDialog  extends AbstractDatastoreDialog<Neo4jDatastore> implements SchemaFactory{
+public class Neo4jDatastoreDialog extends AbstractDatastoreDialog<Neo4jDatastore> implements SchemaFactory {
 
     private static final long serialVersionUID = 1L;
-    
+
     private final JXTextField _hostnameTextField;
-    private final JXTextField _portTextField; 
+    private final JXTextField _portTextField;
     private final JXTextField _usernameTextField;
     private final JPasswordField _passwordTextField;
-    
-    public Neo4jDatastoreDialog(Neo4jDatastore originalDatastore, MutableDatastoreCatalog mutableDatastoreCatalog,
-            WindowContext windowContext, UserPreferences userPreferences) {
-        super(originalDatastore, mutableDatastoreCatalog, windowContext, userPreferences);
-        
+
+    @Inject
+    public Neo4jDatastoreDialog(WindowContext windowContext, MutableDatastoreCatalog catalog,
+            @Nullable Neo4jDatastore originalDatastore, UserPreferences userPreferences) {
+        super(originalDatastore, catalog, windowContext, userPreferences);
+
         setSaveButtonEnabled(false);
 
         _hostnameTextField = WidgetFactory.createTextField();
         _portTextField = WidgetFactory.createTextField();
+        _portTextField.setDocument(new NumberDocument(false));
         _usernameTextField = WidgetFactory.createTextField();
         _passwordTextField = WidgetFactory.createPasswordField(20);
-        
+
         _hostnameTextField.getDocument().addDocumentListener(new DCDocumentListener() {
             @Override
             protected void onChange(DocumentEvent event) {
@@ -79,12 +90,12 @@ public class Neo4jDatastoreDialog  extends AbstractDatastoreDialog<Neo4jDatastor
                 validateAndUpdate();
             }
         });
-        if (originalDatastore == null){
+        if (originalDatastore == null) {
             _hostnameTextField.setText("localhost");
-            _portTextField.setText(""+Neo4jDataContext.DEFAULT_PORT);
+            _portTextField.setText("" + Neo4jDataContext.DEFAULT_PORT);
             _usernameTextField.setText("neo4j");
-            _passwordTextField.setText("neo4j"); 
-        }else{
+            _passwordTextField.setText("neo4j");
+        } else {
             _datastoreNameTextField.setText(originalDatastore.getName());
             _datastoreNameTextField.setEnabled(false);
             _hostnameTextField.setText(originalDatastore.getHostname());
@@ -101,13 +112,13 @@ public class Neo4jDatastoreDialog  extends AbstractDatastoreDialog<Neo4jDatastor
             setStatusError("Please enter a datastore name");
             return false;
         }
-        
+
         final String hostname = _hostnameTextField.getText();
         if (StringUtils.isNullOrEmpty(hostname)) {
             setStatusError("Please enter hostname");
             return false;
         }
-        
+
         final String port = _portTextField.getText();
         if (StringUtils.isNullOrEmpty(port)) {
             setStatusError("Please enter port number");
@@ -124,41 +135,68 @@ public class Neo4jDatastoreDialog  extends AbstractDatastoreDialog<Neo4jDatastor
                 return false;
             }
         }
-        
+
         final String username = _usernameTextField.getText();
         if (StringUtils.isNullOrEmpty(username)) {
             setStatusError("Please enter username space");
             return false;
         }
-        
+
         setStatusValid();
         return true;
     }
+
     @Override
     public String getWindowTitle() {
         return "Neo4j graph database";
     }
+
     @Override
     protected String getBannerTitle() {
         return "Neo4j graph database";
     }
 
     @Override
-    public Schema createSchema() {
-        return null;
+    protected boolean isWindowResizable() {
+        return true;
     }
 
     @Override
+    protected int getDialogWidth() {
+        return 400;
+    }
+
     protected Neo4jDatastore createDatastore() {
-        // TODO Auto-generated method stub
-        return null;
+        final String datastoreName = _datastoreNameTextField.getText();
+        final String hostname = _hostnameTextField.getText();
+        final Integer port = Integer.parseInt(_portTextField.getText());
+        final String username = _usernameTextField.getText();
+        final String password = String.valueOf(_passwordTextField.getPassword());
+        return new Neo4jDatastore(datastoreName, hostname, port.intValue(), username, password);
+    }
+
+    @Override
+    public Schema createSchema() {
+        final Neo4jDatastore datastore = createDatastore();
+        try (final DatastoreConnection connection = datastore.openConnection()) {
+            final Schema defaultSchema = connection.getDataContext().getDefaultSchema();
+            return defaultSchema;
+        }
     }
 
     @Override
     protected String getDatastoreIconPath() {
-      return IconUtils.NEO4J_IMAGEPATH;
+        return IconUtils.NEO4J_IMAGEPATH;
     }
 
-   
+    @Override
+    protected List<Entry<String, JComponent>> getFormElements() {
+        List<Entry<String, JComponent>> result = super.getFormElements();
+        result.add(new ImmutableEntry<String, JComponent>("Hostname", _hostnameTextField));
+        result.add(new ImmutableEntry<String, JComponent>("Port", _portTextField));
+        result.add(new ImmutableEntry<String, JComponent>("Username", _usernameTextField));
+        result.add(new ImmutableEntry<String, JComponent>("Password", _passwordTextField));
+        return result;
+    }
 
 }

4ab9386d4329ab89f4f48dd302d8c40b6503c3d9
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2016-02-25 15:41:48 +0100

    Issue #1151: Fixed

--- components/basic-transformers/src/test/java/org/datacleaner/beans/transform/RemoveSubstringTransformerTest.java
+++ components/basic-transformers/src/test/java/org/datacleaner/beans/transform/RemoveSubstringTransformerTest.java
@@ -29,67 +29,108 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class RemoveSubstringTransformerTest {
 
-    private RemoveSubstringTransformer _t;
+    private RemoveSubstringTransformer _transformer;
     private MockInputRow _inputRow;
+    private InputColumn<String> _stringColumn;
 
     @Before
     public void before() {
         final InputColumn<String> baseColumn = new MockInputColumn<>("baseCol", String.class);
-        final InputColumn<?> stringColumn = new MockInputColumn<>("stringCol", String.class);
+        _stringColumn = new MockInputColumn<>("stringCol", String.class);
         final InputColumn<?> numberColumn = new MockInputColumn<>("numberCol", Number.class);
         final InputColumn<?> listColumn = new MockInputColumn<>("listCol", List.class);
         // Just to test that other types doesn't sent it spinning
         final InputColumn<?> booleanColumn = new MockInputColumn<>("booleanCol", Boolean.class);
 
-        final InputColumn<?>[] subtractColumns =
-                new InputColumn[] { stringColumn, numberColumn, listColumn, booleanColumn };
+        final InputColumn<?>[] subtractColumns = new InputColumn[] { _stringColumn, numberColumn, listColumn,
+                booleanColumn };
 
-        _inputRow = new MockInputRow().put(baseColumn, "hello goodbye, 5, a, 2, c, true, false")
-                .put(subtractColumns[0], "bye").put(subtractColumns[1], 5).put(subtractColumns[2],
-                        Arrays.asList("a", 2, false)).put(subtractColumns[3], true);
+        _inputRow = new MockInputRow().put(baseColumn, "hello goodbye, 5, a, 2, c, true, false").put(_stringColumn,
+                "bye").put(numberColumn, 5).put(listColumn, Arrays.asList("a", 2, false)).put(booleanColumn, true);
 
-        _t = new RemoveSubstringTransformer();
-        _t.baseColumn = baseColumn;
-        _t.substringColumns = subtractColumns;
+        _transformer = new RemoveSubstringTransformer();
+        _transformer.baseColumn = baseColumn;
+        _transformer.substringColumns = subtractColumns;
     }
 
     @Test
     public void testGetOutputColumns() {
-        Assert.assertEquals(1, _t.getOutputColumns().getColumnCount());
-        Assert.assertEquals(String.class, _t.getOutputColumns().getColumnType(0));
-        Assert.assertEquals("baseCol (substring removed)", _t.getOutputColumns().getColumnName(0));
+        Assert.assertEquals(1, _transformer.getOutputColumns().getColumnCount());
+        Assert.assertEquals(String.class, _transformer.getOutputColumns().getColumnType(0));
+        Assert.assertEquals("baseCol (substring removed)", _transformer.getOutputColumns().getColumnName(0));
     }
 
     @Test
-    public void testTransformSimple() {
-        String[] result = _t.transform(_inputRow);
+    public void testTransformSimpleRemoval() {
+        String[] result = _transformer.transform(_inputRow);
         Assert.assertEquals(1, result.length);
         Assert.assertEquals("hello good, , , , c, , flse", result[0]);
     }
 
     @Test
+    public void testTransformSimpleCaseSensitiveNonMatch() {
+        String[] result = _transformer.transform(new MockInputRow().put(_transformer.baseColumn,
+                "GOOD BYE CASE SENSITIVE GUY"));
+        Assert.assertEquals(1, result.length);
+        Assert.assertEquals("GOOD BYE CASE SENSITIVE GUY", result[0]);
+    }
+
+    @Test
+    public void testTransformSimpleNonRemoval() {
+        String[] result = _transformer.transform(new MockInputRow().put(_transformer.baseColumn,
+                "nothing of interest here"));
+        Assert.assertEquals(1, result.length);
+        Assert.assertEquals("nothing of interest here", result[0]);
+    }
+
+    @Test
     public void testTransformNullBaseColumn() {
-        String[] result = _t.transform(new MockInputRow().put(_t.baseColumn, null));
+        String[] result = _transformer.transform(new MockInputRow().put(_transformer.baseColumn, null));
         Assert.assertEquals(1, result.length);
         Assert.assertEquals(null, result[0]);
     }
-    
 
     @Test
     public void testTransformNullReplacementColumn() {
-        String[] result = _t.transform(new MockInputRow().put(_t.baseColumn, "foo"));
+        String[] result = _transformer.transform(new MockInputRow().put(_transformer.baseColumn, "foo"));
         Assert.assertEquals(1, result.length);
         Assert.assertEquals("foo", result[0]);
     }
 
     @Test
     public void testTransformWholeWords() {
-        _t.wholeWordsOnly = true;
-        String[] result = _t.transform(_inputRow);
+        _transformer.wholeWordsOnly = true;
+        String[] result = _transformer.transform(_inputRow);
         Assert.assertEquals(1, result.length);
         Assert.assertEquals("hello goodbye, , , , c, , ", result[0]);
+    }
+
+    @Test
+    public void testTransformCaseInsensitive() throws Exception {
+        _transformer.caseSensitive = false;
+        _transformer.wholeWordsOnly = false;
+
+        String[] result = _transformer.transform(new MockInputRow().put(_transformer.baseColumn, "HELLO GOODBYE DUDE")
+                .put(_stringColumn, "bye"));
+        Assert.assertEquals(1, result.length);
+        Assert.assertEquals("HELLO GOOD DUDE", result[0]);
+    }
+
+    @Test
+    public void testTransformCaseInsensitiveWholeWords() throws Exception {
+        _transformer.caseSensitive = false;
+        _transformer.wholeWordsOnly = true;
+
+        String[] result1 = _transformer.transform(new MockInputRow().put(_transformer.baseColumn, "HELLO GOODBYE DUDE")
+                .put(_stringColumn, "bye"));
+        Assert.assertEquals(1, result1.length);
+        Assert.assertEquals("HELLO GOODBYE DUDE", result1[0]);
+
+        String[] result2 = _transformer.transform(new MockInputRow().put(_transformer.baseColumn, "HELLO GOOD BYE DUDE")
+                .put(_stringColumn, "bye"));
+        Assert.assertEquals(1, result2.length);
+        Assert.assertEquals("HELLO GOOD  DUDE", result2[0]);
     }
 }

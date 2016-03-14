e4e127179c1afd375df36e4a5fcf12206e5bf24a
claudiap <Claudia.Pesu@HumanInference.com>
2016-03-14 13:45:41 +0100

    added default instead of "org.datacleaner.hadoop.environment" in the servers list

--- desktop/ui/src/main/java/org/datacleaner/windows/SelectHadoopConfigurationDialog.java
+++ desktop/ui/src/main/java/org/datacleaner/windows/SelectHadoopConfigurationDialog.java
@@ -7,7 +7,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -24,11 +27,15 @@ import org.datacleaner.configuration.ServerInformationCatalogImpl;
 import org.datacleaner.panels.DCPanel;
 import org.datacleaner.server.DirectConnectionHadoopClusterInformation;
 import org.datacleaner.server.DirectoryBasedHadoopClusterInformation;
+import org.datacleaner.server.EnvironmentBasedHadoopClusterInformation;
+import org.datacleaner.util.HadoopResource;
 import org.datacleaner.util.LookAndFeelManager;
 import org.datacleaner.util.WidgetFactory;
 import org.datacleaner.util.WidgetUtils;
 import org.datacleaner.widgets.DCLabel;
 import org.jfree.ui.tabbedui.VerticalLayout;
+
+
 
 public class SelectHadoopConfigurationDialog extends JComponent {
 
@@ -40,6 +47,7 @@ public class SelectHadoopConfigurationDialog extends JComponent {
     private final JButton _cancelButton;
     private final JButton _optionsButton;
     private String _hadoopConfiguration;
+    private final LinkedList<String> _mappedServers; 
 
     public SelectHadoopConfigurationDialog(ServerInformationCatalog serverInformationCatalog) {
         _serverInformationCatalog = serverInformationCatalog;
@@ -49,7 +57,9 @@ public class SelectHadoopConfigurationDialog extends JComponent {
 
         final DCLabel label = DCLabel.dark("Select Hadoop configuration: ");
 
-        final String[] serverNames = serverInformationCatalog.getServerNames();
+        // It's important to keep the order of the elements. 
+        _mappedServers = new LinkedList<String>();  
+        final String[] serverNames = mapServers(serverInformationCatalog.getServerNames(), _mappedServers);
         _serverList = new JList<String>(serverNames);
         _serverList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
         _serverList.setLayoutOrientation(JList.VERTICAL);
@@ -75,11 +85,27 @@ public class SelectHadoopConfigurationDialog extends JComponent {
         add(panel);
     }
 
+    /**
+     *  We avoid having HadoopResource.DEFAULT_CLUSTERREFERENCE("org.datacleaner.hadoop.environment") as a server name. We write "default" instead
+     */
+    private String[] mapServers(String[] serverNames, LinkedList<String> mappedServers) {
+
+        for (int i = 0; i < serverNames.length; i++) {
+            final String serverName = serverNames[i];
+            if (serverName.equals(HadoopResource.DEFAULT_CLUSTERREFERENCE)) {
+                mappedServers.add("default");
+            } else {
+                mappedServers.add(serverName);
+            }
+
+        }
+        return mappedServers.toArray(new String[serverNames.length]);
+    }
+
     public String selectServer(Component parent, ServerInformationCatalog serverInformationCatalog) {
 
         final SelectHadoopConfigurationDialog selectHadoopConfigurationDialog = new SelectHadoopConfigurationDialog(
                 serverInformationCatalog);
-
         final JDialog dialog = WidgetFactory.createModalDialog(selectHadoopConfigurationDialog, parent,
                 "Select Hadoop Configuration", false);
 
@@ -87,7 +113,8 @@ public class SelectHadoopConfigurationDialog extends JComponent {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                selectHadoopConfigurationDialog._hadoopConfiguration = selectHadoopConfigurationDialog._serverList.getSelectedValue();
+                final int selectedIndex = selectHadoopConfigurationDialog._serverList.getSelectedIndex();
+                selectHadoopConfigurationDialog._hadoopConfiguration = serverInformationCatalog.getServerNames()[selectedIndex];
                 dialog.dispose();
             }
         });
@@ -104,7 +131,7 @@ public class SelectHadoopConfigurationDialog extends JComponent {
     public static void main(String[] args) throws Exception {
         LookAndFeelManager.get().init();
         final List<ServerInformation> servers = new ArrayList<>();
-        //servers.add(new EnvironmentBasedHadoopClusterInformation("default", "hadoop conf dir"));
+        servers.add(new EnvironmentBasedHadoopClusterInformation(HadoopResource.DEFAULT_CLUSTERREFERENCE, "hadoop conf dir"));
         servers.add(new DirectoryBasedHadoopClusterInformation("directory", "directopry set up",
                 "C:\\Users\\claudiap\\git\\vagrant-vms\\bigdatavm\\yarn_conf_client"));
         servers.add(new DirectConnectionHadoopClusterInformation("namenode", "directconnection", new URI(
@@ -115,7 +142,7 @@ public class SelectHadoopConfigurationDialog extends JComponent {
         frame.pack();
         frame.dispose();
 
-        SelectHadoopConfigurationDialog selectHadoopConfigurationDialog = new SelectHadoopConfigurationDialog(serverInformationCatalog); 
+        final SelectHadoopConfigurationDialog selectHadoopConfigurationDialog = new SelectHadoopConfigurationDialog(serverInformationCatalog); 
         
         System.out.println(selectHadoopConfigurationDialog.selectServer(frame, serverInformationCatalog));
 

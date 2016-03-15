8915dd89a63fd33cb4ebc2a49003ef2a9a41c19e
rposkocil <r.poskocil@gmc.net>
2016-03-15 14:43:03 +0100

    Edited preview on right click menu - change default number of records for preview of remote components to 10.

--- desktop/ui/src/main/java/org/datacleaner/widgets/visualization/JobGraphMouseListener.java
+++ desktop/ui/src/main/java/org/datacleaner/widgets/visualization/JobGraphMouseListener.java
@@ -19,6 +19,10 @@
  */
 package org.datacleaner.widgets.visualization;
 
+import edu.uci.ics.jung.algorithms.layout.AbstractLayout;
+import edu.uci.ics.jung.visualization.control.GraphMouseListener;
+import edu.uci.ics.jung.visualization.picking.PickedState;
+
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -46,6 +50,7 @@ import org.datacleaner.bootstrap.WindowContext;
 import org.datacleaner.configuration.DataCleanerConfiguration;
 import org.datacleaner.connection.Datastore;
 import org.datacleaner.data.MetaModelInputColumn;
+import org.datacleaner.descriptors.RemoteTransformerDescriptor;
 import org.datacleaner.job.HasFilterOutcomes;
 import org.datacleaner.job.InputColumnSourceJob;
 import org.datacleaner.job.builder.AnalysisJobBuilder;
@@ -61,10 +66,6 @@ import org.datacleaner.widgets.DescriptorMenuBuilder;
 import org.datacleaner.windows.MetadataDialog;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import edu.uci.ics.jung.algorithms.layout.AbstractLayout;
-import edu.uci.ics.jung.visualization.control.GraphMouseListener;
-import edu.uci.ics.jung.visualization.picking.PickedState;
 
 /**
  * Listener for mouse events on the {@link JobGraph}.
@@ -181,7 +182,11 @@ public class JobGraphMouseListener extends MouseAdapter implements GraphMouseLis
             final TransformerComponentBuilder<?> tjb = (TransformerComponentBuilder<?>) componentBuilder;
             final JMenuItem previewMenuItem = new JMenuItem("Preview data",
                     ImageManager.get().getImageIcon(IconUtils.ACTION_PREVIEW, IconUtils.ICON_SIZE_SMALL));
-            previewMenuItem.addActionListener(new PreviewTransformedDataActionListener(_windowContext, tjb));
+            if(tjb.getDescriptor() instanceof RemoteTransformerDescriptor) {
+                previewMenuItem.addActionListener(new PreviewTransformedDataActionListener(_windowContext, null, tjb, 10));
+            } else {
+                previewMenuItem.addActionListener(new PreviewTransformedDataActionListener(_windowContext, tjb));
+            }
             previewMenuItem.setEnabled(componentBuilder.isConfigured());
             popup.add(previewMenuItem);
         }

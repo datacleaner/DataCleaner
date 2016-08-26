c521e2e2833fe68476a3555d2fd6f8c5033524ca
jhorcicka <jakub.horcicka@humaninference.com>
2016-08-26 10:18:37 +0200

    #1352 Reference data wizards: DatastoreHelper refactoring.

--- monitor/extensions/referencedata-wizards/src/main/java/org/datacleaner/monitor/server/wizard/shared/datastore/DatastoreHelper.java
+++ monitor/extensions/referencedata-wizards/src/main/java/org/datacleaner/monitor/server/wizard/shared/datastore/DatastoreHelper.java
@@ -1,60 +1,62 @@
 package org.datacleaner.monitor.server.wizard.shared.datastore;
 
-import org.apache.metamodel.schema.Column;
+import java.util.List;
+
 import org.apache.metamodel.schema.Schema;
 import org.apache.metamodel.schema.Table;
 import org.datacleaner.connection.Datastore;
 import org.datacleaner.monitor.configuration.TenantContext;
 import org.datacleaner.monitor.shared.model.DatastoreIdentifier;
+import org.datacleaner.monitor.shared.model.HasName;
 
 public class DatastoreHelper {
-    public static String getDatastoreOptions(TenantContext tenantContext) {
-        final StringBuilder builder = new StringBuilder();
-
-        for (DatastoreIdentifier id : tenantContext.getDatastores()) {
-            builder.append(String.format("<option value=\"%s\">%s</option>", id.getName(), id.getName()));
-        }
-
-        return builder.toString();
+    public static String getDatastoreOptions(final TenantContext tenantContext) {
+        final List<DatastoreIdentifier> datastoreIds = tenantContext.getDatastores();
+        
+        return getOptionsHtml(datastoreIds.toArray(new Datastore[datastoreIds.size()]));
     }
 
-    public static String getSchemaOptions(TenantContext tenantContext, String datastoreName) {
-        final StringBuilder builder = new StringBuilder();
-        final DatastoreIdentifier datastoreId = new DatastoreIdentifier(datastoreName);
-        final Datastore datastore = tenantContext.getDatastore(datastoreId);
-
-        for (Schema schema : datastore.openConnection().getSchemaNavigator().getSchemas()) {
-            builder.append(String.format("<option value=\"%s\">%s</option>", schema.getName(), schema.getName()));
-        }
-
-        return builder.toString();
+    public static String getSchemaOptions(final TenantContext tenantContext, final String datastoreName) {
+        return getOptionsHtml(getDatastore(tenantContext, datastoreName).openConnection().getSchemaNavigator()
+                .getSchemas());
     }
 
     public static String getTableOptions(TenantContext tenantContext, String datastoreName, String schemaName) {
-        final StringBuilder builder = new StringBuilder();
-        final DatastoreIdentifier datastoreId = new DatastoreIdentifier(datastoreName);
-        final Datastore datastore = tenantContext.getDatastore(datastoreId);
-        final Schema schema = datastore.openConnection().getSchemaNavigator().getSchemaByName(schemaName);
-
-        for (Table table : schema.getTables()) {
-            builder.append(String.format("<option value=\"%s\">%s</option>", table.getName(), table.getName()));
-        }
-
-        return builder.toString();
+        return getOptionsHtml(getSchema(tenantContext, datastoreName, schemaName).getTables());
     }
 
     public static String getColumnOptions(TenantContext tenantContext, String datastoreName, String schemaName, 
             String tableName) {
-        final StringBuilder builder = new StringBuilder();
-        final DatastoreIdentifier datastoreId = new DatastoreIdentifier(datastoreName);
-        final Datastore datastore = tenantContext.getDatastore(datastoreId);
-        final Schema schema = datastore.openConnection().getSchemaNavigator().getSchemaByName(schemaName);
-        final Table table = schema.getTableByName(tableName);
+        return getOptionsHtml(getTable(tenantContext, datastoreName, schemaName, tableName).getColumns());
+    }
 
-        for (Column column : table.getColumns()) {
-            builder.append(String.format("<option value=\"%s\">%s</option>", column.getName(), column.getName()));
+    private static String getOptionsHtml(final Object[] objects) {
+        final StringBuilder builder = new StringBuilder();
+
+        for (Object item : objects) {
+            final String name = (item instanceof HasName) ? ((HasName)item).getName() : item.toString();
+            builder.append(getOptionHtml(name));
         }
 
         return builder.toString();
+    }
+
+    private static String getOptionHtml(final String name) {
+        return String.format("<option value=\"%s\">%s</option>", name, name);
+    }
+
+    private static Datastore getDatastore(final TenantContext tenantContext, final String datastoreName) {
+        return tenantContext.getDatastore(new DatastoreIdentifier(datastoreName));
+    }
+
+    private static Schema getSchema(final TenantContext tenantContext, final String datastoreName, 
+            final String schemaName) {
+        return getDatastore(tenantContext, datastoreName).openConnection().getSchemaNavigator()
+                .getSchemaByName(schemaName);
+    }
+
+    private static Table getTable(final TenantContext tenantContext, final String datastoreName, 
+            final String schemaName, final String tableName) {
+        return getSchema(tenantContext, datastoreName, schemaName).getTableByName(tableName);
     }
 }

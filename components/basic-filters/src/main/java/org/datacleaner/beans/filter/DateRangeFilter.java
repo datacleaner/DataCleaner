3dc3a945531529a400075a147274af3296f49711
Jakub Horčička <jhorcicka@users.noreply.github.com>
2016-10-14 11:51:50 +0200

    #1441 More flexible DateRangeFilter (#1442)
    * #1441 More flexible DateRangeFilter; in GUI user can specify "now() plus 1d 2m 3y".
    
    * #1441 SingleDatePropertyWidget: change listener was added to "now plus" text field. Now the lastly entered value is not lost by clicking "Close" without any other action.
    
    * #1441 SingleDatePropertyWidget: PropertyChnageListener was replaced by FocusListener (behavior was not as good as expected).
    
    * #1441 SingleDatePropertySettingDialog: a new dialog for setting a date value.
    
    * #1441 SingleDatePropertySettingDialog: content and old functionality.
    
    * #1441 SingleDatePropertyWidget: GridLayout was replaced by BorderLayout so that "change" button has always the same size.
    
    * #1441 Separate dialog for DateRangeFilter: code style updates.
    
    * #1441 SingleDatePropertyWidget: fix for initial null value; not-set text when uninitialized.
    
    * #1441 ShiftedToday: fix related to leading minus sign in the input; SingleDatePropertySettingDialog: nowPlus was renamed to todayPlus. SingleDatePropertyWidget: default value was changed from now() to today().
    
    * #1441 SingleDatePropertySettingDialog: static and dynamic group in UI.
    
    * #1441 DateRangeFilter labels update; in case of "now", "today" and "yesterday" there is the word instead of Date.toString().
    
    * #1441 SingleDateProperty*: groups of elements to change the value were renamed; the dialog window is now closed automatically when a parent's dialog is closed.
    
    * #1441 DateRangeFilter: dynamic values.
    
    * #1441 DateRangeFilter: code style fixes.
    
    * #1441 DateRangeFilter: particular date (from datePicker) is updated on closing the "setValue" dialog.

--- components/basic-filters/src/main/java/org/datacleaner/beans/filter/DateRangeFilter.java
+++ components/basic-filters/src/main/java/org/datacleaner/beans/filter/DateRangeFilter.java
@@ -28,6 +28,10 @@ import org.datacleaner.api.Configured;
 import org.datacleaner.api.Description;
 import org.datacleaner.api.InputColumn;
 import org.datacleaner.components.categories.FilterCategory;
+import org.datacleaner.util.convert.NowDate;
+import org.datacleaner.util.convert.ShiftedToday;
+import org.datacleaner.util.convert.TodayDate;
+import org.datacleaner.util.convert.YesterdayDate;
 
 @Named("Date range")
 @Description("A filter that filters out rows where a date value is outside a specified range")
@@ -54,12 +58,12 @@ public class DateRangeFilter extends AbstractQueryOptimizedRangeFilter<Date> {
 
     @Override
     public Date getHighestValue() {
-        return highestValue;
+        return getDynamicValue(highestValue);
     }
 
     @Override
     public Date getLowestValue() {
-        return lowestValue;
+        return getDynamicValue(lowestValue);
     }
 
     @Override
@@ -70,5 +74,51 @@ public class DateRangeFilter extends AbstractQueryOptimizedRangeFilter<Date> {
     @Override
     public int compare(Date o1, Date o2) {
         return o1.compareTo(o2);
+    }
+
+    @Override
+    public String getSuggestedLabel() {
+        final Date highestValue = getHighestValue();
+        final Date lowestValue = getLowestValue();
+        
+        if (highestValue == null || lowestValue == null) {
+            return null;
+        }
+        
+        final InputColumn<Date> column = getColumn();
+        
+        if (column == null) {
+            return null;
+        }
+        
+        return getDateLabel(lowestValue) + " =< " + column.getName() + " =< " + getDateLabel(highestValue);
+    }
+    
+    private Date getDynamicValue(final Date date) {
+        if (date instanceof NowDate) {
+            return new NowDate();
+        } else if (date instanceof TodayDate){
+            return new TodayDate();
+        } else if (date instanceof YesterdayDate){
+            return new YesterdayDate();
+        } else if (date instanceof ShiftedToday){
+            return new ShiftedToday(((ShiftedToday)date).getInput());
+        } else {
+            return date;
+        }       
+    }
+
+    private String getDateLabel(final Date date) {
+        if (date instanceof NowDate) {
+            return "now";
+        } else if (date instanceof TodayDate){
+            return "today";
+        } else if (date instanceof YesterdayDate){
+            return "yesterday";
+        } else if (date instanceof ShiftedToday){
+            return "today plus [" + ((ShiftedToday)date).getInput() + "]";
+        } else {
+            return date.toString();
+        }
     }
 }

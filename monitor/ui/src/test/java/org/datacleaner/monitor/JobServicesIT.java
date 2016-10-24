f1bb9bd6ddcf7939a9c51cd865eebd28f442fb5d
Jakub Horčička <jhorcicka@users.noreply.github.com>
2016-10-24 10:55:00 +0200

    #1563 docker-testing: jobs from Monitor were added. (#1576)
    * #1563 docker-testing: jobs from Monitor were added.
    
    * Copies of the jobs were removed; available jobs will be tested.

--- monitor/ui/src/test/java/org/datacleaner/monitor/JobServicesIT.java
+++ monitor/ui/src/test/java/org/datacleaner/monitor/JobServicesIT.java
@@ -19,10 +19,6 @@
  */
 package org.datacleaner.monitor;
 
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.equalTo;
-
-import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
@@ -33,6 +29,9 @@ import org.junit.Test;
 import org.junit.rules.ExternalResource;
 
 import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
 
 public class JobServicesIT {
     private static final String JOBS_PATH = "/jobs/";
@@ -51,15 +50,22 @@ public class JobServicesIT {
         RestAssured.authentication = basic(USER_NAME, USER_PASSWORD);
     }
 
-    @Test(timeout = ONE_MINUTE)
-    public void testJobCycle() throws URISyntaxException, InterruptedException {
-        final String jobName = "customer_completeness";
-        final String jobFileName = jobName + ".analysis.xml";
+    @Test(timeout = 5 * ONE_MINUTE)
+    public void testJobs() throws URISyntaxException, InterruptedException {
+        final String[] jobNames = {
+                "Sample custom job",
+                "Copy employees to customer table",
+                "Customer completeness",
+                "Customer profiling",
+                "product_profiling",
+        };
+        
+        for (String name : jobNames) {
+            testJob(name);
+        }
+    }
 
-        given().multiPart(new File(getClass().getClassLoader().getResource(jobFileName).toURI()))
-            .expect().body("file_type", equalTo("ANALYSIS_JOB")).and()
-            .expect().body("status", equalTo("Success")).when().post(JOBS_PATH + jobFileName);
-
+    public void testJob(String jobName) throws URISyntaxException, InterruptedException {
         final String resultPath = "/logs/" + post(JOBS_PATH + jobName + ".trigger").then().extract().path("resultId");
 
         while (get(resultPath).then().extract().path("execution-log.execution-status").equals("PENDING")) {

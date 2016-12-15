47422de0ef029bf50aa20bdceaa57b9aacc206fe
Jakub Neubauer <jakub.neubauer@gmail.com>
2016-12-15 11:10:34 +0100

    Simplified REST API - automatic column mapping by map key (#1575)
    * REST component API - if input is a map and no input columns config is given, try to map the values it to input columns, taking key as a input column property name.
    
    * bugfix: the original configuration was removed if mapping columns automatically
    
    * code clean
    
    * code clean

--- monitor/services/src/main/java/org/datacleaner/monitor/server/components/InputColumnAndMappedPropertyRewriter.java
+++ monitor/services/src/main/java/org/datacleaner/monitor/server/components/InputColumnAndMappedPropertyRewriter.java
@@ -19,8 +19,10 @@
  */
 package org.datacleaner.monitor.server.components;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.datacleaner.api.MappedProperty;
@@ -46,10 +48,12 @@ public class InputColumnAndMappedPropertyRewriter implements InputRewriter {
 
         ConfiguredPropertyDescriptor inputColumnProp = null;
         ConfiguredPropertyDescriptor mappedProp = null;
+        final List<ConfiguredPropertyDescriptor> inputColProperties = new ArrayList<>();
 
         final Set<ConfiguredPropertyDescriptor> props = transformer.getConfiguredProperties();
         for (final ConfiguredPropertyDescriptor prop : props) {
             if (prop.isInputColumn()) {
+                inputColProperties.add(prop);
                 if (prop.isRequired()) {
                     // we have the second required input column property - this not supported by this enricher
                     if (inputColumnProp != null) {
@@ -59,35 +63,112 @@ public class InputColumnAndMappedPropertyRewriter implements InputRewriter {
                 }
             }
         }
-        if (inputColumnProp == null) {
-            // The transformer has no input column property
-            return false;
-        }
 
-        // Check if there is a mapped property for the input column.
-        for (final ConfiguredPropertyDescriptor prop : props) {
-            final MappedProperty mappedPropAnnot = prop.getAnnotation(MappedProperty.class);
-            if (mappedPropAnnot != null) {
-                if (inputColumnProp.getName().equals(mappedPropAnnot.value())) {
-                    if (mappedProp != null) {
-                        // second mapped property for the same inputColumn property.
-                        // (Is this an error?)
-                        return false;
+        if (inputColumnProp != null) {
+            // Check if there is a mapped property for the input column.
+            for (final ConfiguredPropertyDescriptor prop : props) {
+                final MappedProperty mappedPropAnnot = prop.getAnnotation(MappedProperty.class);
+                if (mappedPropAnnot != null) {
+                    if (inputColumnProp.getName().equals(mappedPropAnnot.value())) {
+                        if (mappedProp != null) {
+                            // second mapped property for the same inputColumn property.
+                            // (Is this an error?)
+                            return false;
+                        }
+                        mappedProp = prop;
                     }
-                    mappedProp = prop;
+                }
+            }
+
+            // If mapped property exists and is not configured => let do the MappedPropertyInputEnricher the enrichment
+            if (mappedProp != null) {
+                if (input.configuration == null || !input.configuration.getProperties().containsKey(mappedProp.getName())) {
+                    return enrichMappedProperty(inputColumnProp, mappedProp, input);
                 }
             }
         }
 
-        // If mapped property exists and is not configured => let do the MappedPropertyInputEnricher the enrichment
-        if (mappedProp != null) {
-            if (input.configuration == null || !input.configuration.getProperties().containsKey(mappedProp.getName())) {
-                return enrichMappedProperty(inputColumnProp, mappedProp, input);
-            }
+        if (enrichMapDataObjectsToInputColumnsByName(inputColProperties, input)) {
+            return true;
         }
 
         enrichWithSingleInputProperty(inputColumnProp, input);
         return true;
+    }
+
+    private boolean enrichMapDataObjectsToInputColumnsByName(List<ConfiguredPropertyDescriptor> inputColProperties,
+            ProcessStatelessInput input) {
+        if (inputColProperties.isEmpty()) {
+            return false;
+        }
+        // check if we can convert input "map" to the input columns
+        if (input.data.isArray()) {
+            for (JsonNode item: input.data) {
+                if (!item.isObject()) {
+                    return false;
+                }
+            }
+            // OK, we have array of objects
+        } else if (input.data.isObject()) {
+            // OK, we have one object
+        } else {
+            return false;
+        }
+
+        final JsonNodeFactory json = Serializator.getJacksonObjectMapper().getNodeFactory();
+        if (input.data.isArray()) {
+            final ArrayNode inputData = (ArrayNode)input.data;
+            for (int i = 0; i < inputData.size(); i++) {
+                inputData.set(i++, mapDataObjectToInputColumnsByName(inputData.get(i), inputColProperties));
+            }
+        } else {
+            input.data = json.arrayNode().add(mapDataObjectToInputColumnsByName(input.data, inputColProperties));
+        }
+
+        if (input.configuration == null) {
+            input.configuration = new ComponentConfiguration();
+        }
+
+
+        for (int i = 0; i < inputColProperties.size(); i++) {
+            final String colName = "c" + (i + 1);
+            input.configuration.getProperties().put(inputColProperties.get(i).getName(), json.textNode(colName));
+            input.configuration.getColumns().add(json.textNode(colName));
+        }
+
+        return true;
+    }
+
+    private JsonNode mapDataObjectToInputColumnsByName(JsonNode item, List<ConfiguredPropertyDescriptor> inputColProperties) {
+        final JsonNodeFactory json = Serializator.getJacksonObjectMapper().getNodeFactory();
+        final ArrayNode values = json.arrayNode();
+
+        for (ConfiguredPropertyDescriptor inputColPropDesc: inputColProperties) {
+            JsonNode value = findPropertyByColumnNameOrAlias(item, inputColPropDesc);
+            // nothing found - put null value to the values array
+            if (value == null) {
+                value = json.nullNode();
+            }
+            values.add(value);
+        }
+        return values;
+    }
+
+    private static JsonNode findPropertyByColumnNameOrAlias(JsonNode item, ConfiguredPropertyDescriptor inputColPropDesc) {
+        JsonNode value = item.get(inputColPropDesc.getName());
+        if (value != null) {
+            return value;
+        }
+        final String[] aliases = inputColPropDesc.getAliases();
+        if (aliases != null) {
+            for (String alias : aliases) {
+                value = item.get(alias);
+                if (value != null) {
+                    return value;
+                }
+            }
+        }
+        return null;
     }
 
     private void enrichWithSingleInputProperty(final ConfiguredPropertyDescriptor inputColumnProp,
@@ -97,9 +178,9 @@ public class InputColumnAndMappedPropertyRewriter implements InputRewriter {
         // first repair the input data
         if (input.data.isArray()) {
             if (inputColumnProp.isArray()) {
-                // create single row containing the original data
                 // If all items are already array, do nothing.
-                if (!allItemsAreArray((ArrayNode) input.data)) {
+                // otherwise, create single row containing the original data
+                if (!allItemsAreArray((ArrayNode)input.data)) {
                     input.data = json.arrayNode().add(input.data);
                 }
             } else {

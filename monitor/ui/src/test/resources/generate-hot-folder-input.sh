c5f8302ef673988be75d3406227497b37d35cb56
jakub horcicka <jakub.horcicka@humaninference.com>
2017-01-12 10:27:06 +0100

    Bash scripts were made one-line -- a try for windows.

--- monitor/ui/src/test/resources/generate-hot-folder-input.sh
+++ monitor/ui/src/test/resources/generate-hot-folder-input.sh
@@ -1,11 +1,1 @@
-#!/bin/sh
-
-hot_folder=/tmp/hot_folder
-
-if [ ! -d $hot_folder ]; then
-  mkdir $hot_folder
-fi
-
-path="$hot_folder/input.csv"
-echo "VALUE" > $path
-seq 1 1 111111 >> $path
+hot_folder=/tmp/hot_folder;if [ ! -d $hot_folder ]; then mkdir $hot_folder;fi;path="$hot_folder/input.csv";echo "VALUE" > $path;seq 1 1 111111 >> $path
--- monitor/ui/src/test/resources/get-results-count.sh
+++ monitor/ui/src/test/resources/get-results-count.sh
@@ -1,7 +1,1 @@
-#!/bin/sh
-
-datacleaner_directory=/usr/local/tomcat/webapps/DataCleaner-monitor-ui-*-SNAPSHOT
-results_directory=$datacleaner_directory/repository/demo/results
-job_prefix=simple_numbers_distribution
-
-ls $results_directory | grep $job_prefix | wc -l
+datacleaner_directory=/usr/local/tomcat/webapps/DataCleaner-monitor-ui-*-SNAPSHOT;results_directory=$datacleaner_directory/repository/demo/results;job_prefix=simple_numbers_distribution;ls $results_directory | grep $job_prefix | wc -l

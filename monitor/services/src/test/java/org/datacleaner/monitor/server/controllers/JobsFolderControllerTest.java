769bf7eb9810a7ce9c7f77423a9e5e5d139a3453
Arjan Seijkens <Arjan.Seijkens@humaninference.com>
2017-02-03 11:15:49 +0100

    Added an option to the JobsFolderController, so you can directly define a hot folder for a job which is posted to it. Also added a scheduling endpoint which you can use to schedule an existing job so it uses a hot folder.
    Next to that I updated the dependency on mockito to the latest version to be able to make use of java 8 lambda expressions in the unit tests.

--- monitor/services/src/test/java/org/datacleaner/monitor/server/controllers/JobsFolderControllerTest.java
+++ monitor/services/src/test/java/org/datacleaner/monitor/server/controllers/JobsFolderControllerTest.java
@@ -20,13 +20,27 @@
 package org.datacleaner.monitor.server.controllers;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.ws.rs.core.MediaType;
 
 import org.datacleaner.configuration.DataCleanerEnvironmentImpl;
+import org.datacleaner.monitor.configuration.TenantContext;
 import org.datacleaner.monitor.configuration.TenantContextFactory;
 import org.datacleaner.monitor.configuration.TenantContextFactoryImpl;
+import org.datacleaner.monitor.scheduling.SchedulingService;
+import org.datacleaner.monitor.scheduling.model.ScheduleDefinition;
 import org.datacleaner.monitor.server.job.MockJobEngineManager;
+import org.datacleaner.repository.RepositoryFile;
+import org.datacleaner.repository.RepositoryFolder;
 import org.datacleaner.repository.file.FileRepository;
 import org.junit.Test;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 public class JobsFolderControllerTest {
 
@@ -71,5 +85,42 @@ public class JobsFolderControllerTest {
         final String resultsMetadata4 = controller.getFolderJobsByMetadataProperty("tenant6", "", "").toString();
         assertEquals("[]", resultsMetadata4);
 
+    }
+
+    @Test
+    public void testUploadAnalysisJobToFolderJson() throws Exception {
+        final String jobFileName = "myjob.analysis.xml";
+
+        final TenantContext context = mock(TenantContext.class);
+        final TenantContextFactory contextFactory = mock(TenantContextFactory.class);
+        final RepositoryFolder jobFolder = mock(RepositoryFolder.class);
+        final RepositoryFile jobFile = mock(RepositoryFile.class);
+        final SchedulingService schedulingService = mock(SchedulingService.class);
+        final ScheduleDefinition scheduleDefinition = mock(ScheduleDefinition.class);
+
+        when(contextFactory.getContext("test")).thenReturn(context);
+        when(context.getJobFolder()).thenReturn(jobFolder);
+        when(jobFolder.createFile(eq(jobFileName), any())).thenReturn(jobFile);
+        when(schedulingService.getSchedule(argThat(tenant -> tenant.getId().equals("test")),
+                argThat(job -> job.getName().equals("myjob")))).thenReturn(scheduleDefinition);
+        when(jobFile.getType()).thenReturn(RepositoryFile.Type.ANALYSIS_JOB);
+        when(jobFile.getName()).thenReturn(jobFileName);
+        when(jobFile.getQualifiedPath()).thenReturn("/");
+
+        final JobsFolderController jobsFolderController = new JobsFolderController();
+        jobsFolderController._contextFactory = contextFactory;
+        jobsFolderController._schedulingService = schedulingService;
+
+        final MockMvc mockMvc = MockMvcBuilders.standaloneSetup(jobsFolderController).build();
+
+        final MockMultipartFile multipartFile =
+                new MockMultipartFile("file", jobFileName, MediaType.TEXT_PLAIN, "dummy".getBytes());
+
+        mockMvc.perform(fileUpload("/test/jobs").file(multipartFile).param("hotfolder", "/test"))
+                .andExpect(status().isOk());
+
+        verify(schedulingService, times(1)).updateSchedule(argThat(tenant -> tenant.getId().equals("test")),
+                eq(scheduleDefinition));
+        verify(scheduleDefinition, times(1)).setHotFolder("/test");
     }
 }

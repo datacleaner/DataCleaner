8705e0d9f2fb6f4f2b9d5612c3a8c7f9bfec2e7d
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2019-03-05 11:42:06 -0800

    Moved the UI code out of the extension, into desktop-ui module.

--- desktop/ui/src/main/java/org/datacleaner/panels/machinelearning/MLTrainingAnalyzerPresenterRenderer.java
+++ desktop/ui/src/main/java/org/datacleaner/panels/machinelearning/MLTrainingAnalyzerPresenterRenderer.java
@@ -17,7 +17,7 @@
  * 51 Franklin Street, Fifth Floor
  * Boston, MA  02110-1301  USA
  */
-package org.datacleaner.components.machinelearning.ui;
+package org.datacleaner.panels.machinelearning;
 
 import javax.inject.Inject;
 
